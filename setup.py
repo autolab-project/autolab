2e11e026c9b597d974b9829afc41aac92201523f
qcha41 <q.chateiller@gmail.com>
2019-07-20 21:47:25 +0200

    Package structure modifications

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 17:30:03 2019
+
+@author: qchat
+"""
+
+from distutils.core import setup
+
+setup(
+    name = 'usit',
+    packages = ['usit'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Universal Scanning Interface : Python automation package for scientific laboratory experiments',
+    author = 'Quentin Chateiller',
+    author_email = 'q.chateiller@gmail.com',
+    url = 'https://github.com/qcha41/usit',
+    download_url = 'download link you saved',
+    keywords = ['Universal','Scanning','Interface','automation','scientific','laboratory','experiments','measures'],
+    classifiers = [],
+)
