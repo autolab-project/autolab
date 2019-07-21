796b6b1f18d2437580a1469f6f9617a83c1a2a89
qcha41 <q.chateiller@gmail.com>
2019-07-21 15:02:38 +0200

    c

--- setup.py
+++ setup.py
@@ -5,17 +5,32 @@ Created on Sat Jul 20 17:30:03 2019
 @author: qchat
 """
 
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name = 'usit',
-    packages = ['usit'],
-    data_files = [('usit', ['config/*'])],
     version = '0.1',  # Ideally should be same as your GitHub release tag varsion
-    description = 'Universal Scanning Interface : Python automation package for scientific laboratory experiments',
     author = 'Quentin Chateiller',
     author_email = 'q.chateiller@gmail.com',
-    url = 'https://github.com/qcha41/usit',
+    description = 'Universal Scanning Interface : Python automation package for scientific laboratory experiments',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/qcha41/usit'
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+            
+    data_files = [('usit', ['config/*'])],
+    
+    author = 'Quentin Chateiller',
+    author_email = 'q.chateiller@gmail.com',
+    url = 'https://upload.pypi.org/legacy/',
     download_url = 'https://github.com/qcha41/usit/archive/0.1.tar.gz',
-    keywords = ['Universal','Scanning','Interface','automation','scientific','laboratory','experiments','measures'],
+    keywords = ['Universal','Scanning','Interface','automation','scientific','laboratory','experiments','measures']
 )
+
