709f48ef924c87e1a2e63fbd0d4ec9a24e609284
qcha41 <q.chateiller@gmail.com>
2019-07-21 15:18:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Created on Sat Jul 20 17:30:03 2019
 @author: qchat
 """
 
-from setuptools import setup
+from setuptools import setup,find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -19,18 +19,12 @@ setup(
     long_description = long_description,
     long_description_content_type="text/markdown",
     url = 'https://github.com/qcha41/usit'
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-            
-    data_files = [('usit', ['config/*'])],
-    
-    author = 'Quentin Chateiller',
-    author_email = 'q.chateiller@gmail.com',
-    url = 'https://upload.pypi.org/legacy/',
-    download_url = 'https://github.com/qcha41/usit/archive/0.1.tar.gz',
+    include_package_data=True
     keywords = ['Universal','Scanning','Interface','automation','scientific','laboratory','experiments','measures']
 )
 
