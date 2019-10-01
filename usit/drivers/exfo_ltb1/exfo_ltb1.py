b2b612ea2a7499567001db4cdd443006b6e4477c
qcha41 <q.chateiller@gmail.com>
2019-10-01 21:00:23 +0200

    Update exfo_ltb1.py

--- usit/drivers/exfo_ltb1/exfo_ltb1.py
+++ usit/drivers/exfo_ltb1/exfo_ltb1.py
@@ -47,7 +47,7 @@ class Device():
 ############################## Connections classes ##############################
 class Device_TELNET(Device):
     
-    def __init__(self, address=None, **kwargs):
+    def __init__(self, address='192.168.0.9', **kwargs):
         from telnetlib import Telnet
         
         self.TIMEOUT = 1
