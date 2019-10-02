121449c9675de911ff8180b13a868e61302b4082
bruno066 <bruno.garbin.etu@gmail.com>
2019-10-02 16:30:57 +0200

    gpib

--- usit/drivers/ilxlightwave_ldc3724/ilxlightwave_ldc3724.py
+++ usit/drivers/ilxlightwave_ldc3724/ilxlightwave_ldc3724.py
@@ -68,18 +68,13 @@ class Device_VISA(Device):
         self.controller.write(command)
         time.sleep(0.01)
         
-        
-        
 class Device_GPIB(Device):
-    
-    def __init__(self,address='GPIB0::2::INSTR'):
+    def __init__(self,address=2,board_index=0,**kwargs):
         import Gpib
-        self.inst = Gpib.Gpib(address[0],address[1])
-        Device.__init__(self)
         
-    def close(self):
-        """WARNING: GPIB closing is automatic at sys.exit() doing it twice results in a gpib error"""
-        self.inst.close(self.inst.id)
+        self.inst = Gpib.Gpib(int(address),int(board_index))
+        Device.__init__(self)
+    
     def query(self,query):
         self.write(query)
         return self.read()
@@ -87,6 +82,9 @@ class Device_GPIB(Device):
         self.inst.write(query)
     def read(self):
         return self.inst.read()
+    def close(self):
+        """WARNING: GPIB closing is automatic at sys.exit() doing it twice results in a gpib error"""
+        Gpib.gpib.close(self.inst.id) 
 ############################## Connections classes ##############################
 #################################################################################
 
@@ -365,16 +363,16 @@ class TEC():
         
         
 if __name__ == '__main__':
+    
     from optparse import OptionParser
-    import sys,os
-    
-    ADDRESS = 'GPIB0::26::INSTR'
-    
+    import inspect
+    import sys
+
     usage = """usage: %prog [options] arg
                
                EXAMPLES:
-                   set_ilxlightwaveldc3724 -i GPIB0::1::INSTR -a 30 -t 20.1
-                   set the pump current to 30 mA and the temperature to 20.1 degree celcius
+                   set_ilxlightwaveldc3724 -i 2 -b 0 -a 30 -t 20.1
+                   set the pump current to 30 mA and the temperature to 20.1 degree celcius. The gpib address is set to 19 and the board number to 0.
                """
     parser = OptionParser(usage)
     parser.add_option("-c", "--command", type="str", dest="command", default=None, help="Set the command to use." )
@@ -382,23 +380,27 @@ if __name__ == '__main__':
     parser.add_option("-a", "--current", type="str", dest="current", default=None, help="Set the pump current in mA." )
     parser.add_option("-p", "--power", type="str", dest="power", default=None, help="Set the pump power in ?." )
     parser.add_option("-t", "--temperature", type="str", dest="temperature", default=None, help="Set the locking temperature." )
-    parser.add_option("-i", "--address", type="str", dest="address", default=ADDRESS, help="Set the GPIB address to use to communicate." )
+    parser.add_option("-i", "--address", type="str", dest="address", default='GPIB0::2::INSTR', help="Set the GPIB address to use to communicate." )
+    parser.add_option("-b", "--board_index", type='str', dest="board_index", default='0', help="Set the GPIB address to use to communicate." )
+    parser.add_option("-l", "--link", type="string", dest="link", default='GPIB', help="Set the connection type." )
     (options, args) = parser.parse_args()
     
     ### Start the talker ###
-    I = Device(address=options.address)
+    classes = [name for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass) if obj.__module__ is __name__]
+    assert 'Device_'+options.link in classes , "Not in " + str([a for a in classes if a.startwith('Device_')])
+    Device_LINK = getattr(sys.modules[__name__],'Device_'+options.link)
+    I = Device_LINK(address=options.address,board_index=options.board_index)
+    
     if options.query:
         print('\nAnswer to query:',options.query)
         rep = I.query(options.query)
         print(rep,'\n')
-        try: sys.exit()
-        except: os._exit(1)
+        sys.exit()
     elif options.command:
         print('\nExecuting command',options.command)
         I.write(options.command)
         print('\n')
-        try: sys.exit()
-        except: os._exit(1)
+        sys.exit()
     
     if options.current:
         I.las.setCurrent(options.current)
@@ -410,7 +412,6 @@ if __name__ == '__main__':
         if (eval(options.temperature) > 18) and (eval(options.temperature) < 25):
             I.tec.setTemperature(options.temperature)
     
-    try: sys.exit()
-    except: os._exit(1)
+    sys.exit()
 
         
