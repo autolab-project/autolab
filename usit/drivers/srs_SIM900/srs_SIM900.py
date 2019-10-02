06cd037879be83a8b7083beb136d446f151c22bf
bruno066 <bruno.garbin.etu@gmail.com>
2019-10-02 20:08:42 +0200

    SIM modules

--- usit/drivers/srs_SIM900/srs_SIM900.py
+++ usit/drivers/srs_SIM900/srs_SIM900.py
@@ -1,87 +1,139 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+WARNING: The message processing to submodules is not optimal as connecting/disconnecting streams for each commands: should be optimized.
+
+Supported instruments (identified):
+- 
 
 """
 
-Python module made for controlling srs SIM960 (PID controller) inserted in a SIM900 frame 
-
-"""
-
-import visa as v
-from optparse import OptionParser
-import sys
 import time
 
-ADDRESS = 'GPIB::2::INSTR'
 
 class Device():
-        def __init__(self,address=ADDRESS,query=None,command=None,smart_relock=None,port=None,setpoint=None,auto_lock=None,lock=None,unlock=None):
-            
-            rm = v.ResourceManager('@py')
-            self.PID = rm.get_instrument(address)
-            self.PID.write('CEOI ON')
-            self.PID.write('EOIX ON')
-            
-            self.write('CONN '+port+',"CONAME"')
-            self.write('TERM LF')
-            
-            self.write('*IDN?')
-            print('\nCONNECTING to module on PORT', port,':',self.read())
-            
-            if query:
-                print('\nAnswer to query:',query)
-                self.write(query)
-                rep = self.read()
-                print(rep,'\n')
-                self.exit()
-            elif command:
-                print('\nExecuting command',command)
-                self.write(command)
-                print('\n')
-                self.exit()
-            
-            if lock:
-                self.write('AMAN 1')
-            elif unlock:
-                self.write('AMAN 0')
-            if smart_relock:
-                self.smart_relock(port)
-            if auto_lock:
+    
+    modules = {'Module_SIM960':Module_SIM960}
+    slotNaming = 'slot<NUM> = <MODULE_NAME>,<SLOT_NAME>'
+    
+    def __init__(self):
+        
+        self.write('CEOI ON')
+        self.write('EOIX ON')
+        self.write('TERM LF')
+        
+        # Submodules
+        self.slotnames = []
+        prefix = 'slot'
+        for key in kwargs.keys():
+            if key.startswith(prefix):
+                slot_num = key[len(prefix):]
+                module = modules[ kwargs[key].split(',')[0].strip() ]
+                name = kwargs[key].split(',')[1].strip()
+                setattr(self,name,module(self,slot_num))
+                self.slotnames.append(name)
+        
+        
+    def idn(self):
+        return self.query('*IDN?')
+
+
+#################################################################################
+############################## Connections classes ##############################
+class Device_VISA(Device):
+    def __init__(self, address='GPIB0::2::INSTR',**kwargs):
+        import visa
+        
+        rm = visa.ResourceManager()
+        self.inst = rm.get_instrument(address)
+        
+        Device.__init__(self)
+        
+    def close(self):
+        self.inst.close()
+    def query(self,query):
+        self.write(query)
+        return self.read()
+    def write(self,query):
+        self.inst.write(query)
+    def read(self):
+        rep = self.inst.read()
+        return rep
+    
+class Device_GPIB(Device):
+    def __init__(self,address=2,board_index=0,**kwargs):
+        import Gpib
+        
+        self.inst = Gpib.Gpib(int(address),int(board_index))
+        Device.__init__(self)
+    
+    def query(self,query):
+        self.write(query)
+        return self.read()
+    def write(self,command):
+        self.inst.write(command)
+    def read(self):
+        return self.inst.read()
+    def close(self):
+        """WARNING: GPIB closing is automatic at sys.exit() doing it twice results in a gpib error"""
+        Gpib.gpib.close(self.inst.id)
+############################## Connections classes ##############################
+#################################################################################
+
+
+class Module_SIM960():
+    def __init__(self,dev,slot):
+        self.slot = int(slot)
+        self.dev  = dev
+        
+
+    def send_command_to_slot(self,command):
+        self.dev.write(f'CONN {self.slot},"CONAME"')
+        self.dev.write(command)
+        self.dev.write("CONAME")
+    def get_query_from_slot(self,command):
+        self.dev.write(f'CONN {self.slot},"CONAME"')
+        rep = self.dev.query(command)
+        self.dev.write("CONAME")
+        return rep
+
+    def set_output_manual(self):
+        self.send_command_to_slot('AMAN 0')
+    def set_output_pid(self):
+        self.send_command_to_slot('AMAN 1')
+    def set_output_manual_voltage(self,val):
+        self.send_command_to_slot(f'MOUT {val}')
+    def get_output_voltage(self):
+        return float(self.get_query_from_slot('OMON?'))
+
+    def smart_relock(self,peculiar=False):
+        rep = self.get_output_voltage()
+        if peculiar:                     # port 5
+            if rep<1.5 or rep>8.5:
+                self.re_lock()
+        else:
+            if rep<-3.5 or rep>3.5:
                 self.re_lock(port)
-            if setpoint:
-                self.write('SETP '+setpoint)
-            
-            self.exit()
-        
-        
-        def smart_relock(self,port):
-            self.write('OMON?')
-            rep = eval(self.read())
-            if port=='3':
-                if rep<-3.5 or rep>3.5:
-                    self.re_lock(port)
-            elif port=='5':
-                if rep<1.5 or rep>8.5:
-                    self.re_lock(port)
-        def re_lock(self,port):
-            self.write('AMAN 0')
-            time.sleep(0.1)
-            if port=='3':
-                self.write('MOUT 0')
-            elif port=='5':
-                self.write('MOUT 5')
-            time.sleep(0.1)
-            self.write('AMAN 1')
-        
-        def exit(self):
-            self.write('CONAME')
-            sys.exit()
-        def write(self,query):
-            self.PID.write(query)
-        def read(self):
-            return self.PID.read()
-            
+
+                
+    def re_lock(self):
+        self.set_output_manual()
+        time.sleep(0.1)
+        if peculiar:                     # port 5
+            self.set_output_manual_voltage(5)
+        else:
+            self.set_output_manual_voltage(0)
+        time.sleep(0.1)
+        self.set_output_pid()
+
+
 
 if __name__ == '__main__':
+
+    from optparse import OptionParser
+    import inspect
+    import sys
 
     usage = """usage: %prog [options] arg
                
@@ -99,9 +151,41 @@ if __name__ == '__main__':
     parser.add_option("-u", "--unlock", type="str", dest="unlock", default=None, help="Unlock" )
     parser.add_option("-i", "--port", type="str", dest="port", default='5', help="Port for the PID freme to apply the command to" )
     parser.add_option("-s", "--setpoint", type="str", dest="setpoint", default=None, help="Setpoint value to be used" )
-    parser.add_option("-i", "--address", type="str", dest="address", default=ADDRESS, help="Set gpib address to use for the communication" )
     parser.add_option("-t", "--testout", action = "store_true", dest="testout", default=False, help="Test the output voltage and re-lock if needed" )
+    parser.add_option("-i", "--address", type="str", dest="address", default='2', help="Set gpib address to use for the communication" )
+    parser.add_option("-b", "--board_index", type='str', dest="board_index", default='0', help="Set the GPIB address to use to communicate." )
+    parser.add_option("-l", "--link", type="string", dest="link", default='GPIB', help="Set the connection type." )
+    parser.add_option("-p", "--port", type="str", dest="port", default='5', help="Port for the PID frame to apply the command to" )
     (options, args) = parser.parse_args()
     
     ### Start the talker ###
-    Device(address=options.address,query=options.que,command=options.com,smart_relock=options.testout,port=options.port,setpoint=options.setpoint,auto_lock=options.autolock,lock=options.lock,unlock=options.unlock)
+    classes = [name for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isclass) if obj.__module__ is __name__]
+    assert 'Device_'+options.link in classes , "Not in " + str([a for a in classes if a.startwith('Device_')])
+    Device_LINK = getattr(sys.modules[__name__],'Device_'+options.link)
+    I = Device_LINK(address=options.address,board_index=options.board_index)
+    
+    if query:
+        print('\nAnswer to query:',query)
+        self.write(query)
+        rep = self.read()
+        print(rep,'\n')
+        self.exit()
+    elif command:
+        print('\nExecuting command',command)
+        self.write(command)
+        print('\n')
+        self.exit()
+    
+    if lock:
+        self.write('AMAN 1')
+    elif unlock:
+        self.write('AMAN 0')
+    if smart_relock:
+        self.smart_relock(port)
+    if auto_lock:
+        self.re_lock(port)
+    if setpoint:
+        self.write('SETP '+setpoint)
+    
+    I.close()
+    sys.exit()
