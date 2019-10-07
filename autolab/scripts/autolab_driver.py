7e4952d486d30719913316203255b7d1f2eeedab
bgarbin <52413914+bgarbin@users.noreply.github.com>
2019-10-07 14:37:40 +0200

    Update autolab_driver.py

--- autolab/scripts/autolab_driver.py
+++ autolab/scripts/autolab_driver.py
@@ -6,6 +6,11 @@ Created on Fri May 17 15:04:04 2019
 """
 
 import argparse
-
+import os
 # Pas d'import autolab ici, sinon ca execute tous les __init__ sur le chemin
+
+from ..core import paths
+#os.path.dirname(__file__)
+
+
     
