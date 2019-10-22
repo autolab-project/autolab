2a7b5d5d296f372e30a680bdb4b0bfa0a7e5e4d7
bruno066 <bruno.garbin.etu@gmail.com>
2019-10-22 11:22:17 +0200

    the wind of freedom

--- autolab/drivers/tektronix_DPO4104/tektronix_DPO4104.py
+++ autolab/drivers/tektronix_DPO4104/tektronix_DPO4104.py
@@ -8,7 +8,7 @@ Supported instruments (identified):
 
 import os
 import time
-from numpy import frombuffer,int8
+from numpy import frombuffer,int8,ndarray
 
 
 class Driver():
@@ -52,6 +52,16 @@ class Driver():
         self.write('ACQUIRE:STATE OFF')
     def is_stopped(self):
         return '0' in self.query('ACQUIRE:STATE?')
+    
+    
+    def get_driver_model(self):
+        model = []
+        for i in range(1,self.nb_channels+1):
+            model.append({'element':'module','name':f'channel{i}','object':getattr(self,f'channel{i}'), 'help':'Channels'})
+        model.append({'element':'variable','name':'is_stopped','read':self.is_stopped, 'type':bool,'help':'Query whether scope is stopped'})
+        model.append({'element':'action','name':'stop','do':self.stop,'help':'Set single mode'})
+        model.append({'element':'action','name':'run','do':self.run,'help':'Set single mode'})
+        return model
 
 #################################################################################
 ############################## Connections classes ##############################
@@ -123,3 +133,8 @@ class Channel():
         return array_of_float
 
 
+    def get_driver_model(self):
+        model = []
+        model.append({'element':'variable','name':'trace_raw','type':bytes,'read':self.get_data_raw,'help':'Get the current trace in bytes'})
+        model.append({'element':'variable','name':'trace','type':ndarray,'read':self.get_data,'help':'Get the current trace in a numpy array of integers'})
+        return model
