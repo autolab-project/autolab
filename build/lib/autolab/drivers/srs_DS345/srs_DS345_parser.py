e7fb250e8eb1e6ded8a899fbcde39ce45905c123
Bruno Garbin <bruno@toniq-04.c2n.u-psud.fr>
2019-10-23 14:08:11 +0200

    minor typos and verifs srs_DS345

--- build/lib/autolab/drivers/srs_DS345/srs_DS345_parser.py
+++ build/lib/autolab/drivers/srs_DS345/srs_DS345_parser.py
@@ -40,9 +40,9 @@ usage:    autolab-drivers [options] arg
             """
         parser = ArgumentParser(usage=usage,parents=[parser])
         parser.add_argument("-p", "--phase", type=str, dest="phase", default=None, help="Set the phase value in degree." )
-        parser.add_argument("-o", "--offset", type=str, dest="offset", default=None, help="Set the offset value." )
+        parser.add_argument("-o", "--offset", type=str, dest="offset", default=None, help="Set the offset value in Volts." )
         parser.add_argument("-a", "--amplitude", type=str, dest="amplitude", default=None, help="Set the amplitude. Note: The units can be VP(Vpp), VR (Vrms), or DB (dBm)." )
-        parser.add_argument("-f", "--frequency", type=str, dest="frequency", default=None, help="Set the frequency." )
+        parser.add_argument("-f", "--frequency", type=str, dest="frequency", default=None, help="Set the frequency in Hz." )
         
         return parser
 
