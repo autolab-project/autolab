bd96f6710a61ab0f7aebd77701298a09e770a8d8
Bruno Garbin <bruno@toniq-04.c2n.u-psud.fr>
2019-10-23 14:08:11 +0200

    minor typos and verifs srs_DS345

--- autolab/drivers/srs_DS345/srs_DS345.py
+++ autolab/drivers/srs_DS345/srs_DS345.py
@@ -57,6 +57,7 @@ class Driver_VISA(Driver):
         rep = self.inst.read()
         return rep
     
+    
 class Driver_GPIB(Driver):
     def __init__(self,address=19,board_index=0,**kwargs):
         import Gpib
@@ -70,10 +71,11 @@ class Driver_GPIB(Driver):
     def write(self,query):
         self.inst.write(query)
     def read(self,length=1000000000):
-        return self.inst.read(length)#.decode().replace('\r\n','')
+        return self.inst.read(length).decode().strip('\n')
     def close(self):
         """WARNING: GPIB closing is automatic at sys.exit() doing it twice results in a gpib error"""
-        Gpib.gpib.close(self.inst.id)
+        #Gpib.gpib.close(self.inst.id)
+        pass
 ############################## Connections classes ##############################
 #################################################################################
 
