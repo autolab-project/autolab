ca0c6632328ca5aa239e3982e1a01bc8227369fa
Bruno Garbin <bruno@toniq-04.c2n.u-psud.fr>
2019-10-24 17:35:02 +0200

    lecroy auto amp

--- autolab/drivers/lecroy_WAVEMASTER/lecroy_WAVEMASTER_parser.py
+++ autolab/drivers/lecroy_WAVEMASTER/lecroy_WAVEMASTER_parser.py
@@ -40,7 +40,7 @@ usage:    autolab-drivers [options] arg
     autolab-drivers -D nickname -o my_output_file -f WORD 
     Same as previous will save all channels as None are specified and using the device nickname as defined in local_config.ini. the encoding is set to WORD
     
-    autolab-drivers -D nickname -o my_output_file -a 5,1,3 -b 8
+    autolab-drivers -D nickname -o my_output_file -a 1,3 -b 8
     Vertical autoscale will be enabled for channels 1 and 3. They will both go through the optimization loop 5 times, and aiming 8 vertical divisions.
     
     autolab-drivers -D nickname -m some_methods1,arg1,arg2=23 some_methods2,arg1='test'
@@ -52,8 +52,8 @@ usage:    autolab-drivers [options] arg
         parser.add_argument("-F", "--force",action="store_true", dest="force", default=None, help="Allows overwriting file" )
         parser.add_argument("-t", "--trigger", dest="trigger",action="store_true", help="Trigger the scope once" )
         parser.add_argument("-f", "--format", type=str, dest="format", default=None, help="Change data encoding" )
-        parser.add_argument("-a", "--auto_scale", type=str, dest="auto_scale", default=None, help="To allow auto modification of the vertical gain. Argument is a list of: auto_scale iteration number, all the channels to apply spe mode to. Note if no channel specified, all the channels are corrected. WARNING: Specifying more than one channel to apply auto_scale to will result in different trigger events for the acquired channels.")
-        parser.add_argument("-b", "--auto_fact", type=float, dest="auto_fact", default=None, help="For setting limits of the vertical gain, units are in number of scope divisions here. WARNING: Do not overpass 9 due to a security in the code! WARNING: the number of vertical divisions might depend on the scope (8 or 10 usually)." )
+        parser.add_argument("-a", "--auto_scale", type=str, dest="auto_scale", default=None, help="To allow auto modification of the vertical gain. Argument is a list of all the channels to apply spe mode to. Note if no channel specified. WARNING: Specifying more than one channel to apply auto_scale to will result in different trigger events for the acquired channels.")
+        parser.add_argument("-b", "--auto_fact", type=float, dest="auto_fact", default=None, help="For setting limits of the vertical gain, units are in number of scope divisions here. WARNING: code security avoid traces too close from screen extrema (at 9 divisions) and result in endless looping. WARNING: the number of vertical divisions might depend on the scope (8 or 10 usually)." )
 
         return parser
 
@@ -63,14 +63,14 @@ usage:    autolab-drivers [options] arg
         if args.trigger and not args.filename:
             getattr(self.Instance,'single')()
         if args.auto_scale: # test must be located before args.filename's one
-            for chan in args.auto_scale.split(',')[1:]:
-                getattr(getattr(self.Instance,f'channel{chan}'),'set_autoscale_iter')(args.auto_scale.split(',')[0])
+            for chan in args.auto_scale.split(','):
+                getattr(getattr(self.Instance,f'channel{chan}'),'set_autoscale_enable')()
         if args.auto_fact: # test must be located before args.filename's one
             for chan in range(1,getattr(self.Instance,'nb_channels')+1):
                 getattr(getattr(self.Instance,f'channel{chan}'),'set_autoscale_factor')(args.auto_fact) # all channels instances
         if args.filename:
             acq_channels = args.channels.split(',')    # Sort channels for auto ones to be acquired first
-            for chan_auto in args.auto_scale.split(',')[1:]:
+            for chan_auto in args.auto_scale.split(','):
                 if chan_auto in acq_channels:
                     acq_channels.remove(chan_auto)
                     acq_channels.insert(0,chan_auto)
