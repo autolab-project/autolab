58934feadf1a49f144f2f9df13ec8d0a3c23dbb6
bruno066 <bruno.garbin.etu@gmail.com>
2019-11-18 14:04:49 +0100

    autolab.py update (the best is yet to come)

--- scripts/autolab.py
+++ scripts/autolab.py
@@ -9,64 +9,253 @@ Created on Fri May 17 15:04:04 2019
 import os
 import sys
 import autolab
+import argparse
 
 
-def print_help_commands():
+def print_help():
     print()
-    print(f'Usage:')
-    print(f'  autolab [-h] <command> ...')
+    print('Usage:')
+    print('  autolab [-h] <command> ...')
     print()
-    print(f'Commands:')
-    print(f'  gui                   Open the Graphical User Interface')
-    print(f'  driver                Shell scripting')
-    print(f'  device                A device interface')
-    print(f'  doc                   Open the online documentation (readthedocs)')
-    print(f'  report                Open the online report/suggestions webpage (github)')
-    print(f'  infos                 Displays the avalaible drivers and local configurations')
+    print('Commands:')
+    print('  gui                   Start the Graphical User Interface')
+    print('  driver                Driver interface')
+    print('  device                Device interface')
+    print('  doc                   Open the online documentation (readthedocs)')
+    print('  report                Open the online report/suggestions webpage (github)')
+    print('  infos                 Displays the avalaible drivers and local configurations')
     print()
-    print(f'General Options:')
-    print(f'  -h, --help            Show this help message')
+    print('General Options:')
+    print('  -h, --help            Show this help message')
     print()
 
-def import_module(name):
-    module = __import__(name)
-    globals()[module] = module
-    return module
 
 def main() :
-    args               = sys.argv
-    available_commands = ['gui','driver','device','doc','report','infos']  #WARNING add manually to help too
-        
-    # print help
+    
+    args = sys.argv
+    
+    # No command provided or -h/--help option : print help
     if len(args)==1 or args[1]=='-h' or args[1]=='--help':
-        print_help_commands()
+        print_help()
+        
+    else :
+        command = args[1]   # first is 'autolab'
+        
+        # Update the sys.argv for parsers
+        args = [f'autolab {command}'] + args[2:]  # first is 'autolab' and second is command 
+        sys.argv = args
+        
+        if command=='doc':   # Open help on read the docs
+            autolab.doc()
+        elif command=='report':        # Open github report issue webpage
+            autolab.report()
+        elif command=='gui':           # GUI
+            autolab.gui()
+        elif command=='infos':           # GUI
+            autolab.infos()
+        elif command=='driver':
+            driver_parser(args)
+        elif command=='device':
+            device_parser(args)
+        else :
+            print(f"Command {command} not known. Autolab doesn't have Super Cow Power... yet ^^")
+              
+            
+    sys.exit()
+        
+
+def process_config(args_list):
+    
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-D", "--driver", type=str, dest="driver", help="Set the nickname or driver to use: 1) uses nickname if it is defined in local_config.ini OR(if it is not) 2) Set the driver name to use." )
+    parser.add_argument("-C", "--connection", type=str, dest="connection", help="Set the connection type to use for the connection." )
+    parser.add_argument("-A", "--address", type=str, dest="address", help="Set the address to use for the communication." )
+    parser.add_argument("-P","--port", type=int, dest="port", help="Argument used to address different things depending on the connection type. SOCKET: the port number used to communicate, GPIB: the gpib board index, DLL: the path to the dll library." )
+    parser.add_argument("-O","--other", nargs='+', dest="other", help="Set other parameters (slots,...)." )
+
+    args, unknown = parser.parse_known_args(args_list)
+    
+    config = {}
+    if args.connection is not None : config['connection'] = args.connection
+    if args.address is not None : config['address'] = args.address
+    if args.port is not None : config['port'] = args.port
+    if args.other is not None : 
+        for part in args.other : 
+            part = part.replace(' ','')
+            config[part.split('=')[0]] = part.split('=')[1]
+    
+    # Help for autolab driver/device -h/--help and autolab driver/device -h/--help -D driver_name
+    if not args.driver or (not args.driver and args_list[1]=='-h' or args_list[1]=='--help'): print_help_parser(parser,args_list); sys.exit()
+    if args.driver and (args_list[1]=='-h' or args_list[1]=='--help'): 
+        assert args.driver in autolab.list_drivers(), f"Driver {args.driver} not known"
+        autolab.config_help(args.driver,_parser=True)
         sys.exit()
     
-    # Check the provided command
-    if len(args)>1:
-        assert args[1] in available_commands, f"Command {args[1]} not known. Autolab doesn't have Super Cow Power... yet ^^"
-    if len(args)>2:
-        assert args[2] not in available_commands, f"Please provide one command only"
+    return args.driver, config, parser
+
+def print_help_parser(parser,args_list):
+    parser.usage = f"""
+
+----------------  General informations:  ----------------
+
+This is a very basic help message for usage of {args_list[0]}. More info can be found on read the doc website. To display a more extensive further help please have a look at the section help below.
     
-    command = args[1]   # first is autolab
-    if command=='doc':   # Open help on read the docs
-        autolab.doc()
-    elif command=='report':        # Open github report issue webpage
-        autolab.report()
-    elif command=='gui':           # GUI
-        autolab.gui()
-    elif command=='infos':           # GUI
-        autolab.infos()
-    else :                         # autolab_driver.py or autolab_device.py
-        # modify sys.argv
-        sys.argv = [f'autolab_{command}.py'] + args[2:]  # first is 'autolab' and second is command
+    Usage:   {args_list[0]} -D driver_name -C connection -A address -h
+
+Recquired connection arguments (capital letters):
+    -D driver_name: name of the driver to use (e.g.: agilent_33220A). driver_name can be either the driver_name or the defined nickname, as defined by the user in the local_config.ini. See lower for the list of available drivers.
+    -C connection type to use to communicate with the device (e.g.: VISA, VXI11, SOCKET, TELNET, USB, GPIB, ...). You may access the available connections types with an help (see below helps section).
+    -A address: full address to reach the device that depends on the connection type (e.g.: 192.168.0.2  [for VXI11]) and on how you configured the device.
+    
+    
+----------------  Helps (-h option)  ----------------       
+
+Three helps are configured:
+    {args_list[0]} -h
+    This help message.
+    
+    {args_list[0]} -h -D driver_name
+    Short message displaying the device category as well as the implemented connections to a device (VISA, etc).
+    
+    {args_list[0]} -D driver_name -C connection -A address -h
+    Full help message about the driver.
+    
+    
+----------------  List of available drivers (-D option) ----------------
+ 
+    {autolab.infos()}"""
+    
+    parser.print_help()
+    
+    
+def build_driver_infos_for_usage(driver_name,driver_instance):
+    driver_lib = autolab.load_driver_lib(driver_name)
+    
+    # build necessary list
+    list_connection_classes = autolab.get_connection_names(driver_lib)
+    list_additional_classes = autolab.get_module_names(driver_lib)
+    list_instance_methods_and_args = autolab.get_instance_methods(driver_instance)
+    
+    mess = ''
+    mess += '\n    Available connections types (-C option):\n'
+    for connection in list_connection_classes: 
+        mess += f'     - {connection}\n'
+    mess += '\n'
+    mess += '\n    Available additional modules:\n'
+    if list_additional_classes==[]:
+        mess += f'     {list_additional_classes}'
+    else:
+        for additional_class in list_additional_classes: 
+            mess += f'     - {additional_class}\n'
+    mess += '\n'
+    mess += '\n    Available methods (with arguments):'
+    for method in list_instance_methods_and_args:
+        mess += f'\n     - {method[0]}({",".join(method[1])})'
+    mess += '\n'
+    
+    return mess
+    
+    
+def driver_parser(args_list):
+    # Reading of connection information
+    driver_name, config, parser = process_config(args_list)
+    
+    # Reading of methods
+    parser.add_argument("-m", "--methods", nargs='+', dest="methods", help="Set the methods to use." )
+    parser.add_argument("-h", "--help", dest="help", help="Print this help message." )
+    
+    # Instantiation of driver.py and driver_utilities.py
+    global driver_instance
+    driver_instance           = autolab.get_driver(driver_name,**config)
+    driver_utilities          = autolab.load_driver_utilities_lib(driver_name+'_utilities')
+    driver_utilities_instance = driver_utilities.Driver_parser(driver_instance,driver_name)
+    
+    # Add arguments to the existing parser (driver dependant)
+    parser = driver_utilities_instance.add_parser_arguments(parser)
+    
+    # Add help and usage to the parser only if "-h" options requested
+    if '-h' in args_list:
+        dirver_infos_for_usage = build_driver_infos_for_usage(driver_name,driver_instance)
+        parser.usage = driver_utilities_instance.add_parser_usage(dirver_infos_for_usage)
+        parser.usage = parser.usage + f"""
+    autolab-drivers -D nickname -m some_methods1(arg1,arg2=23) some_methods2(arg1='test')
+    Execute some_methods of the driver. A list of available methods is present at the top of this help along with arguments definition.
+                """
+        parser.print_help()
+        sys.exit()
         
-        # execute the appropriate module
-        scripts_path = os.path.join(os.path.dirname(os.path.dirname(autolab.paths.DRIVER_SOURCES['main'])),'scripts')
-        sys.path.insert(0,scripts_path)
-        module = import_module(f'autolab_{command}')
-        module.main()
+    args = parser.parse_args()
 
+    # Finally, execute functions according to the arguments provided
+    driver_utilities_instance.do_something(args)
+    
+    # Process given methods
+    if args.methods:
+        global message
+        for method in args.methods:
+            message = None
+            #assert method in methods_list, f"Method not known or bound. Methods known are: {method_list}"
+            print(f'\nExecuting command:  {method}')
+            exec(f"message = driver_instance.{method}",globals())
+            if message is not None: print(f'Return:  {message}\n')
+    
+    # Liste python des drivers : autolab.list_drivers()
+    # Liste python des classes d'un module : autolab.get_connection_names('dummy')
+    # Liste python des configurations locales : autolab.list_local_configs()
+    # Affichage des drivers + catégories + local config : autolab.infos()
+    # Affichage de l'aide pour la configuration d'un driver : autolab.config_help(driver_name)
+    # Affichage de la liste des fonctions (et leur args) d'une instance : autolab.explore_driver(instance)
+    
+    
+    # Driver closing
+    driver_utilities_instance.exit()
+    
+    
+def device_parser(args_list):
+    
+    # autolab-device -D mydummy -e amplitude -p C:\Users\               GET AND SAVE VARIABLE VALUE
+    # autolab-device -D mydummy -e something                            EXECUTE ACTION
+    # autolab-device -D mydummy -e amplitude -v 4                       SET VARIABLE VALUE
+    # autolab-device -D mydummy -e channel1.amplitude -h                DISPLAY ELEMENT HELP
 
+    # Reading of connection information
+    driver_name, config, config_parser = process_config(args_list)
+    
+    # Parser configuration
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-e', '--element', type=str, dest="element", help='Address of the element to open' )
+    parser.add_argument("-v", "--value", type=str, dest="value", help='Value to set')
+    parser.add_argument("-p", "--path", type=str, dest="path", help='Path where to save data')
+    parser.add_argument("-h", "--help", action='store_true', dest="help", help='Display element help')
+    args, unknown = parser.parse_known_args(args_list)
+
+    # Instantiation
+    instance = autolab.get_device(driver_name,**config)
+    element = instance
+    
+    # Open element
+    if args.element is not None :
+        for name in args.element.split('.') :
+            element = getattr(element,name)
+        
+    # Execute order
+    if args.help is True : print(element.help())
+        
+    elif args.path is not None: 
+        assert element._element_type == 'variable', f"This element is not a Variable"
+        value = element()
+        element.save(args.path,value=value)
+        
+    elif args.value is not None :
+        if element._element_type == 'variable' : element(args.value)
+        elif element._element_type == 'action' : element(args.value)
+        
+    else :
+        assert element._element_type in ['variable','action'], f"Please provide a Variable or Action element"
+        if element._element_type == 'variable' : print(element())
+        elif element._element_type == 'action' : element()
+    
+    instance.close()
+    
 if __name__ == '__main__' : 
     main()
