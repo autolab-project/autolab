51d418f92b12c25a5f6c104ac70b5b0b6da4d503
bruno066 <bruno.garbin.etu@gmail.com>
2020-03-05 10:36:24 +0100

    autolab.report to community.ddns and release notes to community

--- autolab/core/web.py
+++ autolab/core/web.py
@@ -8,7 +8,7 @@ Created on Fri Sep 20 17:11:57 2019
 import webbrowser
 
 def report():
-    webbrowser.open('https://github.com/qcha41/autolab/issues')
+    webbrowser.open('https://autolab-community.ddns.net')
     
 def doc():
     webbrowser.open('https://autolab.readthedocs.io')
