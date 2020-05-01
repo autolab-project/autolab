9bdea49aa5afeabba5fa69a3cb4b257e224a7d30
qcha41 <q.chateiller@gmail.com>
2020-05-01 18:00:53 +0200

    n

--- autolab/core/paths.py
+++ autolab/core/paths.py
@@ -7,6 +7,8 @@ Created on Fri Sep 20 17:11:57 2019
 
 import os
 
+VERSION = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'version.txt')
+
 USER_FOLDER = os.path.join(os.path.expanduser('~'),'autolab')
 USER_LAST_CUSTOM_FOLDER = os.path.expanduser('~')
 DEVICES_CONFIG = os.path.join(USER_FOLDER,'devices_config.ini')
