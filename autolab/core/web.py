cca1fb91050c78b49082e2f315cfcb8349246913
qcha41 <q.chateiller@gmail.com>
2020-05-01 17:30:52 +0200

    web and init

--- autolab/core/web.py
+++ autolab/core/web.py
@@ -7,8 +7,8 @@ Created on Fri Sep 20 17:11:57 2019
 
 import webbrowser
 
-def report():
-    webbrowser.open('https://autolab-community.ddns.net')
+def community():
+    webbrowser.open('https://autolab-community.org')
 
 def doc():
     webbrowser.open('https://autolab.readthedocs.io')
