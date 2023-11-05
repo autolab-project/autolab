autolab-project/autolab

commit e67899f087825d62cf39c4117b7e097f2f5271f1

Add init and end recipe feature
- It is now possible to add, using the drag/drop option, an init and/or an end recipe to the scanner. Not yet available using right click in control panel (Not sure it will).
- Measured variables in the init and end recipe are saved in the same data file as the scan, leading to nan values.
- Changed minValue and maxValue of the scanner figure to account for full nan arrays. (Need to check whether ct400 crashes autolab with np.nan functions).
- Changed autolab version

autolab/core/config.py
@@ -142,11 +142,13 @@ def check_autolab_config():
 
     # Check scanner configuration
     scanner_dict = {'precision': 15,
+                    'recipe_size': [0, 500, 0],
                     }
     if 'scanner' in autolab_config.sections():
-        autolab_config['scanner'] = scanner_dict
         if 'precision' in autolab_config['scanner'].keys() :
             scanner_dict['precision'] = autolab_config['scanner']['precision']
+        if 'recipe_size' in autolab_config['scanner'].keys() :
+            scanner_dict['recipe_size'] = autolab_config['scanner']['recipe_size']
     autolab_config['scanner'] = scanner_dict
 
     # # Check plotter configuration

autolab/core/gui/scanning/data.py
@@ -208,7 +208,11 @@ class Dataset():
         self.gui = gui
         self.config = config
 
-        self.data = pd.DataFrame()
+        recipes_list = self.config['initrecipe'] + self.config['recipe'] + self.config['endrecipe']
+        self.header = ["id", self.config['parameter']['name']
+                       ] + [step['name'] for step in recipes_list if step['element'].type in [int,float,bool]]
+        self.data = pd.DataFrame(columns=self.header)
+
         self.list_array = list()
         self.tempFolderPath = tempfile.mkdtemp() # Creates a temporary directory for this dataset
         self.new = True
@@ -265,7 +269,8 @@ class Dataset():
             if resultName == self.config['parameter']['name'] :
                 element = self.config['parameter']['element']
             else :
-                element = [step['element'] for step in self.config['recipe'] if step['name']==resultName][0]
+                recipes_list = self.config['initrecipe'] + self.config['recipe'] + self.config['endrecipe']
+                element = [step['element'] for step in recipes_list if step['name']==resultName][0]
 
             resultType = element.type
 
@@ -283,16 +288,13 @@ class Dataset():
                     self.list_array.append(folderPath)
 
         self.all_data_temp.append(simpledata)
-        self.data = pd.DataFrame(self.all_data_temp)
-
+        self.data = pd.DataFrame(self.all_data_temp, columns=self.header)
 
         if ID == 1 :
-            self.data = self.data[list(simpledata.keys())] # reorder columns
-            header = True
+            self.data.tail(1).to_csv(os.path.join(self.tempFolderPath,'data.txt'),index=False,mode='a',header=self.header)
         else :
-            header = False
+            self.data.tail(1).to_csv(os.path.join(self.tempFolderPath,'data.txt'),index=False,mode='a', header=False)
 
-        self.data.tail(1).to_csv(os.path.join(self.tempFolderPath,'data.txt'),index=False,mode='a',header=header)
 
 
 

autolab/core/gui/scanning/config.py
@@ -97,7 +97,9 @@ class ConfigManager :
         self.config['step'] = 1
 
         self.config['log'] = False
+        self.config['initrecipe'] = []
         self.config['recipe'] = []
+        self.config['endrecipe'] = []
 
         self.historic = ConfigHistory()
         self._activate_historic = True
@@ -106,11 +108,11 @@ class ConfigManager :
     # NAMES
     ###########################################################################
 
-    def getNames(self,option=None):
+    def getNames(self,option=None, recipe_name='recipe'):
 
         """ This function returns a list of the names of the recipe step and of the parameter """
 
-        names = [step['name'] for step in self.config['recipe']]
+        names = [step['name'] for step in self.config[recipe_name]]
         if self.config['parameter']['element'] is not None and option != 'recipe':
             names.append(self.config['parameter']['name'])
         return names
@@ -121,7 +123,7 @@ class ConfigManager :
 
         """ This function adds a number next to basename in case this basename is already taken """
 
-        names = self.getNames()
+        names = self.getNames(recipe_name='initrecipe') + self.getNames(recipe_name='recipe') + self.getNames(recipe_name='endrecipe')
         name = basename
 
         compt = 0
@@ -175,7 +177,7 @@ class ConfigManager :
         self.gui.parameterManager.refresh()
 
 
-    def addRecipeStep(self,stepType,element,name=None,value=None):
+    def addRecipeStep(self,stepType,element,name=None,value=None, recipe_name='recipe'):
 
         """ This function add a step to the scan recipe """
 
@@ -199,62 +201,69 @@ class ConfigManager :
                         value = False
                 step['value'] = value
 
-            self.config['recipe'].append(step)
-            self.gui.recipeManager.refresh()
+            self.config[recipe_name].append(step)
+            self.refreshRecipe(recipe_name)
             self.gui.dataManager.clear()
             self.addNewConfig()
 
+    def refreshRecipe(self, recipe_name='recipe'):
+        if recipe_name == "initrecipe":
+            self.gui.recipeManager_begin.refresh()
+        if recipe_name == "recipe":
+            self.gui.recipeManager.refresh()
+        if recipe_name == "endrecipe":
+            self.gui.recipeManager_end.refresh()
 
-    def delRecipeStep(self,name):
+    def delRecipeStep(self,name, recipe_name='recipe'):
 
         """ This function removes a step from the scan recipe """
 
         if self.gui.scanManager.isStarted() is False :
-            pos = self.getRecipeStepPosition(name)
-            self.config['recipe'].pop(pos)
-            self.gui.recipeManager.refresh()
+            pos = self.getRecipeStepPosition(name, recipe_name)
+            self.config[recipe_name].pop(pos)
+            self.refreshRecipe(recipe_name)
             self.gui.dataManager.clear()
             self.addNewConfig()
 
 
-    def renameRecipeStep(self,name,newName):
+    def renameRecipeStep(self,name,newName, recipe_name='recipe'):
 
         """ This function rename a step in the scan recipe """
 
         if self.gui.scanManager.isStarted() is False :
             if newName != name :
-                pos = self.getRecipeStepPosition(name)
+                pos = self.getRecipeStepPosition(name, recipe_name)
                 newName = self.getUniqueName(newName)
-                self.config['recipe'][pos]['name'] = newName
-                self.gui.recipeManager.refresh()
+                self.config[recipe_name][pos]['name'] = newName
+                self.refreshRecipe(recipe_name)
                 self.gui.dataManager.clear()
                 self.addNewConfig()
 
 
-    def setRecipeStepValue(self,name,value):
+    def setRecipeStepValue(self,name,value, recipe_name='recipe'):
 
         """ This function set the value of a step in the scan recipe """
 
         if self.gui.scanManager.isStarted() is False :
-            pos = self.getRecipeStepPosition(name)
-            if value != self.config['recipe'][pos]['value'] :
-                self.config['recipe'][pos]['value'] = value
-                self.gui.recipeManager.refresh()
+            pos = self.getRecipeStepPosition(name, recipe_name)
+            if value != self.config[recipe_name][pos]['value'] :
+                self.config[recipe_name][pos]['value'] = value
+                self.refreshRecipe(recipe_name)
                 self.gui.dataManager.clear()
                 self.addNewConfig()
 
 
-    def setRecipeOrder(self,stepOrder):
+    def setRecipeOrder(self,stepOrder, recipe_name='recipe'):
 
         """ This function reorder the recipe as a function of the list of step names provided """
 
         if self.gui.scanManager.isStarted() is False :
-            newOrder = [self.getRecipeStepPosition(name) for name in stepOrder]
-            recipe = self.config['recipe']
-            self.config['recipe'] = [recipe[i] for i in newOrder]
+            newOrder = [self.getRecipeStepPosition(name, recipe_name) for name in stepOrder]
+            recipe = self.config[recipe_name]
+            self.config[recipe_name] = [recipe[i] for i in newOrder]
             self.gui.dataManager.clear()
             self.addNewConfig()
-        self.gui.recipeManager.refresh()
+        self.refreshRecipe(recipe_name)
 
 
     def setNbPts(self,value):
@@ -351,38 +360,38 @@ class ConfigManager :
 
 
 
-    def getRecipeStepElement(self,name):
+    def getRecipeStepElement(self,name, recipe_name='recipe'):
 
         """ This function returns the element of a recipe step """
 
-        pos = self.getRecipeStepPosition(name)
-        return self.config['recipe'][pos]['element']
+        pos = self.getRecipeStepPosition(name, recipe_name)
+        return self.config[recipe_name][pos]['element']
 
 
 
-    def getRecipeStepType(self,name):
+    def getRecipeStepType(self,name, recipe_name='recipe'):
 
         """ This function returns the type a recipe step """
 
-        pos = self.getRecipeStepPosition(name)
-        return self.config['recipe'][pos]['stepType']
+        pos = self.getRecipeStepPosition(name, recipe_name)
+        return self.config[recipe_name][pos]['stepType']
 
 
 
-    def getRecipeStepValue(self,name):
+    def getRecipeStepValue(self,name, recipe_name='recipe'):
 
         """ This function returns the value of a recipe step """
 
-        pos = self.getRecipeStepPosition(name)
-        return self.config['recipe'][pos]['value']
+        pos = self.getRecipeStepPosition(name, recipe_name)
+        return self.config[recipe_name][pos]['value']
 
 
 
-    def getRecipeStepPosition(self,name):
+    def getRecipeStepPosition(self,name, recipe_name='recipe'):
 
         """ This function returns the position of a recipe step in the recipe """
 
-        return [i for i, step in enumerate(self.config['recipe']) if step['name'] == name][0]
+        return [i for i, step in enumerate(self.config[recipe_name]) if step['name'] == name][0]
 
 
 
@@ -418,11 +427,11 @@ class ConfigManager :
 
 
 
-    def getRecipe(self):
+    def getRecipe(self, recipe_name='recipe'):
 
         """ This function returns the whole recipe of the scan """
 
-        return self.config['recipe']
+        return self.config[recipe_name]
 
 
 
@@ -487,19 +496,26 @@ class ConfigManager :
         configPars['parameter']['end_value'] = str(self.config['range'][1])
         configPars['parameter']['log'] = str(int(self.config['log']))
 
-        configPars['recipe'] = {}
-        for i in range(len(self.config['recipe'])) :
-            configPars['recipe'][f'{i+1}_name'] = self.config['recipe'][i]['name']
-            configPars['recipe'][f'{i+1}_stepType'] = self.config['recipe'][i]['stepType']
-            configPars['recipe'][f'{i+1}_address'] = self.config['recipe'][i]['element'].address()
-            stepType = self.config['recipe'][i]['stepType']
-            if stepType == 'set' or (stepType == 'action' and self.config['recipe'][i]['element'].type in [int,float,str,pd.DataFrame,np.ndarray]) :
-                value = self.config['recipe'][i]['value']
-                try:
-                    valueStr = f'{value:.{self.precision}g}'
-                except:
-                    valueStr = f'{value}'
-                configPars['recipe'][f'{i+1}_value'] = valueStr
+        def create_configRecipe(recipe_name):
+            configPars[recipe_name] = {}
+            for i in range(len(self.config[recipe_name])) :
+                configPars[recipe_name][f'{i+1}_name'] = self.config[recipe_name][i]['name']
+                configPars[recipe_name][f'{i+1}_stepType'] = self.config[recipe_name][i]['stepType']
+                configPars[recipe_name][f'{i+1}_address'] = self.config[recipe_name][i]['element'].address()
+                stepType = self.config[recipe_name][i]['stepType']
+                if stepType == 'set' or (stepType == 'action' and self.config[recipe_name][i]['element'].type in [int,float,str,pd.DataFrame,np.ndarray]) :
+                    value = self.config[recipe_name][i]['value']
+                    try:
+                        valueStr = f'{value:.{self.precision}g}'
+                    except:
+                        valueStr = f'{value}'
+                    configPars[recipe_name][f'{i+1}_value'] = valueStr
+
+        if len(self.config['initrecipe']) != 0:
+            create_configRecipe('initrecipe')
+        create_configRecipe('recipe')
+        if len(self.config['endrecipe']) != 0:
+            create_configRecipe('endrecipe')
 
         return configPars
 
@@ -566,67 +582,125 @@ class ConfigManager :
                     config['step'] = 0
 
 
-            config['recipe'] = []
+            def load_configRecipe(configPars, config, recipe_name):
 
-            while True:
-                step = {}
+                config[recipe_name] = []
 
-                i = len(config['recipe'])+1
+                if not configPars.has_section(recipe_name):
+                    return
 
-                if f'{i}_name' in configPars['recipe']:
+                while True:
+                    step = {}
 
-                    step['name'] = configPars['recipe'][f'{i}_name']
-                    name = step['name']
+                    i = len(config[recipe_name])+1
 
-                    assert f'{i}_stepType' in configPars['recipe'], f"Missing stepType in step {i} ({name})."
-                    step['stepType'] = configPars['recipe'][f'{i}_stepType']
+                    if f'{i}_name' in configPars[recipe_name]:
 
-                    assert f'{i}_address' in configPars['recipe'], f"Missing address in step {i} ({name})."
-                    address = configPars['recipe'][f'{i}_address']
-                    element = devices.get_element_by_address(address)
-                    assert element is not None, f"Address {address} not found for step {i} ({name})."
-                    step['element'] = element
+                        step['name'] = configPars[recipe_name][f'{i}_name']
+                        name = step['name']
 
-                    if step['stepType']=='set' or (step['stepType'] == 'action' and element.type in [int,float,str,pd.DataFrame,np.ndarray]) :
-                        assert f'{i}_value' in configPars['recipe'], f"Missing value in step {i} ({name})."
-                        value = configPars['recipe'][f'{i}_value']
+                        assert f'{i}_stepType' in configPars[recipe_name], f"Missing stepType in step {i} ({name})."
+                        step['stepType'] = configPars[recipe_name][f'{i}_stepType']
 
-                        try:
+                        assert f'{i}_address' in configPars[recipe_name], f"Missing address in step {i} ({name})."
+                        address = configPars[recipe_name][f'{i}_address']
+                        element = devices.get_element_by_address(address)
+                        assert element is not None, f"Address {address} not found for step {i} ({name})."
+                        step['element'] = element
+
+                        if step['stepType']=='set' or (step['stepType'] == 'action' and element.type in [int,float,str,pd.DataFrame,np.ndarray]) :
+                            assert f'{i}_value' in configPars[recipe_name], f"Missing value in step {i} ({name})."
+                            value = configPars[recipe_name][f'{i}_value']
+
                             try:
-                                assert self.checkVariable(value) == 0, "Need $eval: to evaluate the given string"
-                            except:
-                                # Type conversions
-                                if element.type in [int]:
-                                    value = int(value)
-                                elif element.type in [float] :
-                                    value = float(value)
-                                elif element.type in [str] :
-                                    value = str(value)
-                                elif element.type in [bool]:
-                                    if value == "False": value = False
-                                    elif value == "True": value = True
-                                    value = int(value)
-                                    assert value in [0,1]
-                                    value = bool(value)
-                                else:
+                                try:
                                     assert self.checkVariable(value) == 0, "Need $eval: to evaluate the given string"
-                        except:
-                            raise ValueError(f"Error with {i}_value = {value}. Expect either {element.type} or device address. Check address or open device first.")
-                        step['value'] = value
+                                except:
+                                    # Type conversions
+                                    if element.type in [int]:
+                                        value = int(value)
+                                    elif element.type in [float] :
+                                        value = float(value)
+                                    elif element.type in [str] :
+                                        value = str(value)
+                                    elif element.type in [bool]:
+                                        if value == "False": value = False
+                                        elif value == "True": value = True
+                                        value = int(value)
+                                        assert value in [0,1]
+                                        value = bool(value)
+                                    else:
+                                        assert self.checkVariable(value) == 0, "Need $eval: to evaluate the given string"
+                            except:
+                                raise ValueError(f"Error with {i}_value = {value}. Expect either {element.type} or device address. Check address or open device first.")
+                            step['value'] = value
+                        else:
+                            step['value'] = None
+
+                        config[recipe_name].append(step)
+
+                    step = {}
+
+                    i = len(config[recipe_name])+1
+
+                    if f'{i}_name' in configPars[recipe_name]:
+
+                        step['name'] = configPars[recipe_name][f'{i}_name']
+                        name = step['name']
+
+                        assert f'{i}_stepType' in configPars[recipe_name], f"Missing stepType in step {i} ({name})."
+                        step['stepType'] = configPars[recipe_name][f'{i}_stepType']
+
+                        assert f'{i}_address' in configPars[recipe_name], f"Missing address in step {i} ({name})."
+                        address = configPars[recipe_name][f'{i}_address']
+                        element = devices.get_element_by_address(address)
+                        assert element is not None, f"Address {address} not found for step {i} ({name})."
+                        step['element'] = element
+
+                        if step['stepType']=='set' or (step['stepType'] == 'action' and element.type in [int,float,str,pd.DataFrame,np.ndarray]) :
+                            assert f'{i}_value' in configPars[recipe_name], f"Missing value in step {i} ({name})."
+                            value = configPars[recipe_name][f'{i}_value']
+
+                            try:
+                                try:
+                                    assert self.checkVariable(value) == 0, "Need $eval: to evaluate the given string"
+                                except:
+                                    # Type conversions
+                                    if element.type in [int]:
+                                        value = int(value)
+                                    elif element.type in [float] :
+                                        value = float(value)
+                                    elif element.type in [str] :
+                                        value = str(value)
+                                    elif element.type in [bool]:
+                                        if value == "False": value = False
+                                        elif value == "True": value = True
+                                        value = int(value)
+                                        assert value in [0,1]
+                                        value = bool(value)
+                                    else:
+                                        assert self.checkVariable(value) == 0, "Need $eval: to evaluate the given string"
+                            except:
+                                raise ValueError(f"Error with {i}_value = {value}. Expect either {element.type} or device address. Check address or open device first.")
+                            step['value'] = value
+                        else:
+                            step['value'] = None
+
+                        config[recipe_name].append(step)
+
                     else:
-                        step['value'] = None
+                        break
 
-                    config['recipe'].append(step)
-
-
-                else:
-                    break
-
+            load_configRecipe(configPars, config, 'initrecipe')
+            load_configRecipe(configPars, config, 'recipe')
+            load_configRecipe(configPars, config, 'endrecipe')
 
             self.config = config
             self.gui.dataManager.clear()
             self.gui.parameterManager.refresh()
-            self.gui.recipeManager.refresh()
+            self.refreshRecipe('initrecipe')
+            self.refreshRecipe('recipe')
+            self.refreshRecipe('endrecipe')
             self.gui.rangeManager.refresh()
 
 

autolab/core/gui/controlcenter/main.py
@@ -350,12 +350,12 @@ class ControlCenter(QtWidgets.QMainWindow):
         self.scanner.configManager.setParameter(variable)
 
 
-    def addStepToScanRecipe(self,stepType,element):
+    def addStepToScanRecipe(self,stepType,element, recipe_name='recipe'):
 
         if self.scanner is None :
             self.openScanner()
 
-        self.scanner.configManager.addRecipeStep(stepType,element)
+        self.scanner.configManager.addRecipeStep(stepType,element, recipe_name=recipe_name)
 
 
     def clearScanner(self):

autolab/core/gui/plotting/figure.py
@@ -9,6 +9,7 @@ Created on Oct 2022
 import os
 import math as m
 
+import numpy as np
 import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -139,8 +140,11 @@ class FigureManager :
 
         datas = [getattr(curve,f'get_{axe}data')() for curve in self.curves]
         if len(datas) > 0 :
-            minValue = min([min(data) for data in datas])
-            maxValue = max([max(data) for data in datas])
+            min_list = [np.nanmin(data) for data in datas if ~np.isnan(data).all()]
+            max_list = [np.nanmax(data) for data in datas if ~np.isnan(data).all()]
+            minValue = np.nanmin(min_list) if ~np.isnan(min_list).all() else 0
+            maxValue = np.nanmax(max_list) if ~np.isnan(max_list).all() else 0
+
             if (minValue,maxValue) != self.getRange(axe) :
                 self.setRange(axe,(minValue,maxValue))
 

autolab/core/gui/scanning/figure.py
@@ -9,6 +9,7 @@ Created on Sun Sep 29 18:12:24 2019
 import os
 import math as m
 
+import numpy as np
 import matplotlib
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -109,9 +110,12 @@ class FigureManager :
 
         datas = [getattr(curve,f'get_{axe}data')() for curve in self.curves]
         if len(datas) > 0 :
-            minValue = min([min(data) for data in datas])
-            maxValue = max([max(data) for data in datas])
-            if (minValue,maxValue) != self.getRange(axe) :
+            min_list = [np.nanmin(data) for data in datas if ~np.isnan(data).all()]
+            max_list = [np.nanmax(data) for data in datas if ~np.isnan(data).all()]
+            minValue = np.nanmin(min_list) if ~np.isnan(min_list).all() else 0
+            maxValue = np.nanmax(max_list) if ~np.isnan(max_list).all() else 0
+
+            if (minValue,maxValue) != self.getRange(axe):
                 self.setRange(axe,(minValue,maxValue))
 
             self.toolbar.update()

commit b41912cad9b50de5f13d723fc8cad2184a011500

fixe scanner recipe bug + modify plotter config
- Fixe scanner recipe that was saving/plotting any variables instead of only measured ones (bug introduced in commit e67899f about init and end recipe)
- Add the device address to the plotter config and rework plotter config

autolab/__init__.py
@@ -20,6 +20,7 @@ del version_adapter
 from .core import config as _config
 _config.initialize_local_directory()
 _config.check_autolab_config()
+_config.check_plotter_config()
 
 # Statistics
 from .core import stats as _stats

autolab/core/config.py
@@ -43,9 +43,7 @@ def initialize_local_directory():
 
     # PLOTTER CONFIGURATION FILE
     if os.path.exists(paths.PLOTTER_CONFIG) is False :
-        plotter_config = configparser.ConfigParser()
-        plotter_config['plugin'] = {'plotter':'plotter'}
-        save_config('plotter',plotter_config)
+        save_config('plotter',configparser.ConfigParser())
         print(f'The configuration file plotter_config.ini has been created: {paths.PLOTTER_CONFIG}')
 
 def save_config(config_name,config):
@@ -201,10 +199,34 @@ def get_scanner_config():
     return get_config('scanner')
 
 
-# def get_plotter_config():
-#     ''' Returns section plotter from autolab_config.ini '''
+# ==============================================================================
+# PLOTTER CONFIG
+# ==============================================================================
 
-#     return get_config('plotter')
+def check_plotter_config():
+
+    """ This function checks config file structures """
+
+    plotter_config = load_config('plotter')
+
+    # Check plugin configuration
+    plugin_dict = {'plotter': 'plotter'}
+    if 'plugin' in plotter_config.sections():
+        plugin_dict = dict()
+        for plugin_name in plotter_config['plugin'].keys():
+            plugin_dict[plugin_name] = plotter_config['plugin'][plugin_name]
+    plotter_config['plugin'] = plugin_dict
+
+    # Check device configuration
+    device_dict = {'address': 'dummy.array_1D'}
+    if 'device' in plotter_config.sections():
+        if 'address' in plotter_config['device'].keys():
+            device_dict['address'] = plotter_config['device']['address']
+
+    plotter_config['device'] = device_dict
+
+    save_config('plotter',plotter_config)
+
 
 
 
@@ -218,7 +240,7 @@ def get_all_devices_configs():
     ''' Returns current devices configuration '''
 
     config = load_config('devices')
-    assert len(set(config.sections())) == len(config.sections()), f"Each device must have a unique name."
+    assert len(set(config.sections())) == len(config.sections()), "Each device must have a unique name."
 
     return config
 

autolab/core/gui/plotting/data.py
@@ -20,6 +20,7 @@ except:
 from PyQt5 import QtCore, QtWidgets
 
 from ... import paths
+from ... import config
 
 
 def find_delimiter(filename):
@@ -132,7 +133,8 @@ class DataManager :
 
         self.overwriteData = True
 
-        self.deviceValue = "ct400.scan.data"
+        plotter_config = config.load_config("plotter")
+        self.deviceValue = str(plotter_config['device']['address'])
 
     def _clear(self):
         self.datasets = []

autolab/core/gui/scanning/data.py
@@ -210,7 +210,7 @@ class Dataset():
 
         recipes_list = self.config['initrecipe'] + self.config['recipe'] + self.config['endrecipe']
         self.header = ["id", self.config['parameter']['name']
-                       ] + [step['name'] for step in recipes_list if step['element'].type in [int,float,bool]]
+                       ] + [step['name'] for step in recipes_list if step['stepType'] == 'measure' and step['element'].type in [int,float,bool]]
         self.data = pd.DataFrame(columns=self.header)
 
         self.list_array = list()