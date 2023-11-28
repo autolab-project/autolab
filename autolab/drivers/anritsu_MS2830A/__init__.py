0968456b1a9dfe006d57fad10664250339baa855
Python-simulation <46069237+Python-simulation@users.noreply.github.com>
2023-11-29 00:42:18 +0100

    remove drivers, they are now in repo autolab-drivers
    Drivers are now here: https://github.com/Python-simulation/autolab-drivers

--- autolab/drivers/anritsu_MS2830A/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  5 23:35:23 2021
-
-@author: jonathan Peltier
-"""
-
--- autolab/drivers/plotter/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 13 21:26:10 2022
-
-@author: jonathan
-"""
-
