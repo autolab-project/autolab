autolab-project/autolab

commit 9ed2e6691f3f6ee05e3ba5d549860ab2f38154ea

several fixes + changes to Variable
- readme: add pypi and doc badges
- fixe: remove possibility to import a config using 'recent import' during a scan
- Change names for Autolab - Scanner, Plotter and Monitor
- fixe bug in pyside2 due to len(layout) instead of layout.count()
- fixe bug if using custom parameter with one point (now use create_array to force array dimension)
- can now have a monitor switching from monitoring a float in time to monitor an array (only useful for Variable if change definition)

Modify Variable:
- now force the use of Variable (can't use $eval.ID anymore but should use $eval:ID() instead)
- can monitor Variable with right click
- fixe impossibility to use $eval in variable with currently non-existing device or variable name
- now raise error if start scan with bad parameter $eval
- A Variable is not longer recreated everytime a set_variable is called (or rename), useful to keep track of variable (monitor for example)

autolab/core/gui/monitoring/data.py
@@ -46,6 +46,11 @@ class DataManager:
         """ This function either replace list by array or add point to list depending on datapoint type """
         y = point[1]
 
+        self.gui.figureManager.setLabel('x', 'x')
+        self.gui.windowLength_lineEdit.hide()
+        self.gui.windowLength_label.hide()
+        self.gui.dataDisplay.hide()
+
         if isinstance(y, np.ndarray):
             if len(y.T.shape) == 1 or y.T.shape[0] == 2:
                 self._addArray(y.T)
@@ -55,6 +60,11 @@ class DataManager:
             self._addArray(y.values.T)
         else:
             self._addPoint(point)
+
+            self.gui.figureManager.setLabel('x', 'Time [s]')
+            self.gui.windowLength_lineEdit.show()
+            self.gui.windowLength_label.show()
+            self.gui.dataDisplay.show()
 
     def _addImage(self, image: np.ndarray):
         """ Add image to ylist data as np.ndarray """
@@ -79,6 +89,8 @@ class DataManager:
 
     def _addPoint(self, point: Tuple[float, float]):
         """ This function append a datapoint [x,y] in the lists of data """
+        if not hasattr(self.xlist, 'append'): self.clear()  # avoid error when switching from array to point
+
         x, y = point
 
         # Append data

autolab/core/gui/scanning/data.py
@@ -129,7 +129,9 @@ class DataManager:
                             if isinstance(values, str):
                                 nbpts *= 11  # OPTIMIZE: can't know length in this case without doing eval (should not do eval here because can imagine recipe_2 with param set at end of recipe_1)
                                 self.gui.progressBar.setStyleSheet("""QProgressBar::chunk {background-color: orange;}""")
-                            else: nbpts *= len(values)
+                            else:
+                                values = utilities.create_array(values)
+                                nbpts *= len(values)
                         else: nbpts *= len(parameter['values'])
                     else: nbpts *= parameter['nbpts']
 

autolab/core/gui/monitoring/main.py
@@ -32,7 +32,7 @@ class Monitor(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         ui_path = os.path.join(os.path.dirname(__file__), 'interface.ui')
         uic.loadUi(ui_path, self)
-        self.setWindowTitle(f"AUTOLAB Monitor : Variable {self.variable.name}")
+        self.setWindowTitle(f"AUTOLAB - Monitor: Variable {self.variable.address()}")
         self.setWindowIcon(QtGui.QIcon(icons['monitor']))
         # Queue
         self.queue = queue.Queue()
@@ -41,20 +41,14 @@ class Monitor(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.sync)
 
         # Window length
-        if self.variable.type in [int, float]:
-            self.xlabel = 'Time [s]'
-            self.windowLength_lineEdit.setText('10')
-            self.windowLength_lineEdit.returnPressed.connect(self.windowLengthChanged)
-            self.windowLength_lineEdit.textEdited.connect(lambda: setLineEditBackground(
-                self.windowLength_lineEdit, 'edited', self._font_size))
-            setLineEditBackground(
-                self.windowLength_lineEdit, 'synced', self._font_size)
-        else:
-            self.xlabel = 'x'
-            self.windowLength_lineEdit.hide()
-            self.windowLength_label.hide()
-            self.dataDisplay.hide()
+        self.windowLength_lineEdit.setText('10')
+        self.windowLength_lineEdit.returnPressed.connect(self.windowLengthChanged)
+        self.windowLength_lineEdit.textEdited.connect(lambda: setLineEditBackground(
+            self.windowLength_lineEdit, 'edited', self._font_size))
+        setLineEditBackground(
+            self.windowLength_lineEdit, 'synced', self._font_size)
 
+        self.xlabel = ''  # defined in data according to data type
         self.ylabel = f'{self.variable.address()}'  # OPTIMIZE: could depend on 1D or 2D
 
         if self.variable.unit is not None:

autolab/core/gui/plotting/main.py
@@ -69,7 +69,7 @@ class Plotter(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         ui_path = os.path.join(os.path.dirname(__file__),'interface.ui')
         uic.loadUi(ui_path,self)
-        self.setWindowTitle("AUTOLAB Plotter")
+        self.setWindowTitle("AUTOLAB - Plotter")
         self.setWindowIcon(QtGui.QIcon(icons['plotter']))
 
         # Loading of the different centers

autolab/core/gui/scanning/config.py
@@ -825,17 +825,18 @@ class ConfigManager:
 
         var_to_save = {}
         for var_name in names_var_to_save:
-            if var is not None:
-                value = var.raw if isinstance(var, variables.Variable) else var
             var = variables.get_variable(var_name)
 
-                if isinstance(value, np.ndarray): valueStr = array_to_str(
+            if var is not None:
+                assert variables.is_Variable(var)
+                value_raw = var.raw
+                if isinstance(value_raw, np.ndarray): valueStr = array_to_str(
                         value, threshold=1000000, max_line_width=9000000)
-                elif isinstance(value, pd.DataFrame): valueStr = dataframe_to_str(
+                elif isinstance(value_raw, pd.DataFrame): valueStr = dataframe_to_str(
                         value, threshold=1000000)
-                elif isinstance(value, (int, float, str)):
-                    try: valueStr = f'{value:.{self.precision}g}'
-                    except: valueStr = f'{value}'
+                elif isinstance(value_raw, (int, float, str)):
+                    try: valueStr = f'{value_raw:.{self.precision}g}'
+                    except: valueStr = f'{value_raw}'
 
                 var_to_save[var_name] = valueStr
 
@@ -845,29 +846,30 @@ class ConfigManager:
 
     def import_configPars(self, filename: str, append: bool = False):
         """ Import a scan configuration from file with filename name """
-        if os.path.exists(filename):
-            try:
-                legacy_configPars = configparser.ConfigParser()
-                legacy_configPars.read(filename)
-            except:
+        if not self.gui.scanManager.isStarted():
+            if os.path.exists(filename):
                 try:
-                    with open(filename, "r") as read_file:
-                        configPars = json.load(read_file)
-                except Exception as error:
-                    self.gui.setStatus(f"Impossible to load configuration file: {error}", 10000, False)
-                    return None
+                    legacy_configPars = configparser.ConfigParser()
+                    legacy_configPars.read(filename)
+                except:
+                    try:
+                        with open(filename, "r") as read_file:
+                            configPars = json.load(read_file)
+                    except Exception as error:
+                        self.gui.setStatus(f"Impossible to load configuration file: {error}", 10000, False)
+                        return None
+                else:
+                    print("ConfigParser depreciated, now use json. Will convert this config to json if save it.")
+                    configPars = {s: dict(legacy_configPars.items(s)) for s in legacy_configPars.sections()}
+
+                path = os.path.dirname(filename)
+                paths.USER_LAST_CUSTOM_FOLDER = path
+
+                self.load_configPars(configPars, append=append)
+
+                if not self._got_error: self.addNewConfig()
             else:
-                print("ConfigParser depreciated, now use json. Will convert this config to json if save it.")
-                configPars = {s: dict(legacy_configPars.items(s)) for s in legacy_configPars.sections()}
-
-            path = os.path.dirname(filename)
-            paths.USER_LAST_CUSTOM_FOLDER = path
-
-            self.load_configPars(configPars, append=append)
-
-            if not self._got_error: self.addNewConfig()
-        else:
-            self.gui.setStatus(f"Configuration file {filename} doesn't exists", 5000)
+                self.gui.setStatus(f"Configuration file {filename} doesn't exists", 5000)
 
     def load_configPars(self, configPars: dict, append: bool = False):
         """ Creates a config representing a scan form a configPars """

autolab/core/gui/scanning/main.py
@@ -34,7 +34,7 @@ class Scanner(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         ui_path = os.path.join(os.path.dirname(__file__), 'interface.ui')
         uic.loadUi(ui_path, self)
-        self.setWindowTitle("AUTOLAB Scanner")
+        self.setWindowTitle("AUTOLAB - Scanner")
         self.setWindowIcon(QtGui.QIcon(icons['scanner']))
         self.splitter.setSizes([500, 700])  # Set the width of the two main widgets
         self.setAcceptDrops(True)
@@ -225,7 +225,7 @@ class Scanner(QtWidgets.QMainWindow):
         self.recipeDict[recipe_name]['parameterManager'][param_name] = new_ParameterManager
 
         layoutAll = self.recipeDict[recipe_name]['recipeManager']._layoutAll
-        layoutAll.insertWidget(len(layoutAll)-1, new_ParameterManager.mainFrame)
+        layoutAll.insertWidget(layoutAll.count()-1, new_ParameterManager.mainFrame)
 
         self._updateSelectParameter()
         self.selectParameter_comboBox.setCurrentIndex(self.selectParameter_comboBox.count()-1)

autolab/core/gui/scanning/parameter.py
@@ -614,8 +614,9 @@ class ParameterManager:
                 values = raw_values
             elif not variables.has_variable(raw_values):
                 values = variables.eval_safely(raw_values)
-                values = create_array(values)
-                assert len(values) != 0, "Cannot have empty array"
+                if not isinstance(values, str):
+                    values = create_array(values)
+                    assert len(values) != 0, "Cannot have empty array"
 
             self.gui.configManager.setValues(self.recipe_name, self.param_name, raw_values)
         except Exception as e:

autolab/core/gui/scanning/scan.py
@@ -98,6 +98,7 @@ class ScanManager:
             self.gui.clear_pushButton.setEnabled(False)
             self.gui.progressBar.setValue(0)
             self.gui.importAction.setEnabled(False)
+            self.gui.openRecentMenu.setEnabled(False)
             self.gui.undo.setEnabled(False)
             self.gui.redo.setEnabled(False)
             self.gui.setStatus('Scan started!', 5000)
@@ -144,6 +145,7 @@ class ScanManager:
         self.gui.clear_pushButton.setEnabled(True)
         self.gui.displayScanData_pushButton.setEnabled(True)
         self.gui.importAction.setEnabled(True)
+        self.gui.openRecentMenu.setEnabled(True)
         self.gui.configManager.updateUndoRedoButtons()
         self.gui.dataManager.timer.stop()
         self.gui.dataManager.sync() # once again to be sure we grabbed every data
@@ -239,8 +241,12 @@ class ScanThread(QtCore.QThread):
 
             if 'values' in parameter:
                 paramValues = parameter['values']
-                paramValues = variables.eval_variable(paramValues)
-                paramValues = create_array(paramValues)
+                try:
+                    paramValues = variables.eval_variable(paramValues)
+                    paramValues = create_array(paramValues)
+                except Exception as e:
+                    self.errorSignal.emit(e)
+                    self.stopFlag.set()
             else:
                 startValue, endValue = parameter['range']
                 nbpts = parameter['nbpts']

commit 9eb4a7226fea4849116badf8ff53e67c7b5fbdbf

monitor fixe
- fixe bug with monitor 'window length' not changeable (bug introduced in 9ed2e66)
- can now monitor numpy array with len(shape) = 0 (one point)
- now raise error and pause if set bad data type in monitor
- now check visibility before show() and hide()

autolab/core/gui/monitoring/data.py
@@ -46,25 +46,28 @@ class DataManager:
         """ This function either replace list by array or add point to list depending on datapoint type """
         y = point[1]
 
-        self.gui.figureManager.setLabel('x', 'x')
-        self.gui.windowLength_lineEdit.hide()
-        self.gui.windowLength_label.hide()
-        self.gui.dataDisplay.hide()
+        if isinstance(y, (np.ndarray, pd.DataFrame)):
+            if self.gui.windowLength_lineEdit.isVisible():
+                self.gui.figureManager.setLabel('x', 'x')
+                self.gui.windowLength_lineEdit.hide()
+                self.gui.windowLength_label.hide()
+                self.gui.dataDisplay.hide()
 
-        if isinstance(y, np.ndarray):
-            if len(y.T.shape) == 1 or y.T.shape[0] == 2:
-                self._addArray(y.T)
-            else:
-                self._addImage(y)  # Defined which axe is major using pg.setConfigOption('imageAxisOrder', 'row-major') in gui start-up so no need to .T data
-        elif isinstance(y, pd.DataFrame):
-            self._addArray(y.values.T)
+            if isinstance(y, np.ndarray):
+                if len(y.T.shape) in (0, 1) or y.T.shape[0] == 2:
+                    self._addArray(y.T)
+                else:
+                    self._addImage(y)  # Defined which axe is major using pg.setConfigOption('imageAxisOrder', 'row-major') in gui start-up so no need to .T data
+            elif isinstance(y, pd.DataFrame):
+                self._addArray(y.values.T)
         else:
-            self._addPoint(point)
+            if not self.gui.windowLength_lineEdit.isVisible():
+                self.gui.figureManager.setLabel('x', 'Time [s]')
+                self.gui.windowLength_lineEdit.show()
+                self.gui.windowLength_label.show()
+                self.gui.dataDisplay.show()
 
-            self.gui.figureManager.setLabel('x', 'Time [s]')
-            self.gui.windowLength_lineEdit.show()
-            self.gui.windowLength_label.show()
-            self.gui.dataDisplay.show()
+            self._addPoint(point)
 
     def _addImage(self, image: np.ndarray):
         """ Add image to ylist data as np.ndarray """
@@ -72,9 +75,12 @@ class DataManager:
         self.ylist = image
 
     def _addArray(self, array: np.ndarray):
-        """ This function replace an dataset [x,y] x is time y is array"""
-
-        if len(array.shape) == 1:
+        """ This function replace an dataset [x,y] x is time y is array """
+        if len(array.shape) == 0:
+            y_array = array
+            self.xlist = np.array([0])
+            self.ylist = np.array([y_array])
+        elif len(array.shape) == 1:
             y_array = array
             # Replace data
             self.xlist = np.arange(len(y_array))
@@ -98,7 +104,7 @@ class DataManager:
         self.ylist.append(y)
 
         # Remove too old data (regarding the window length)
-        while max(self.xlist)-min(self.xlist) > self.windowLength:
+        while (max(self.xlist) - min(self.xlist)) > self.windowLength:
             self.xlist.pop(0)
             self.ylist.pop(0)
 

autolab/core/gui/monitoring/figure.py
@@ -50,17 +50,19 @@ class FigureManager:
     # PLOT DATA
     ###########################################################################
 
-    def update(self, xlist: list, ylist: list):
+    def update(self, xlist: list, ylist: list) -> None:
         """ This function update the figure in the GUI """
         if xlist is None: # image
-            self.fig.hide()
-            self.gui.min_checkBox.hide()
-            self.gui.mean_checkBox.hide()
-            self.gui.max_checkBox.hide()
-            self.figMap.show()
+            if self.fig.isVisible():
+                self.fig.hide()
+                self.gui.min_checkBox.hide()
+                self.gui.mean_checkBox.hide()
+                self.gui.max_checkBox.hide()
+                self.figMap.show()
             self.figMap.setImage(ylist)
             return None
-        else:
+
+        if not self.fig.isVisible():
             self.fig.show()
             self.gui.min_checkBox.show()
             self.gui.mean_checkBox.show()
@@ -68,7 +70,13 @@ class FigureManager:
             self.figMap.hide()
 
         # Data retrieval
-        self.plot.setData(xlist, ylist)
+        try:
+            self.plot.setData(xlist, ylist)
+        except Exception as e:
+            self.gui.setStatus(f'Error: {e}', 10000, False)
+            if not self.gui.monitorManager.isPaused():
+                self.gui.pauseButtonClicked()
+            return None
 
         xlist, ylist = self.plot.getData()
 

autolab/core/gui/scanning/figure.py
@@ -240,15 +240,16 @@ class FigureManager:
         self.setLabel('x', variable_x)
         self.setLabel('y', variable_y)
 
-        self.gui.frame_axis.show()
         if data_name == "Scan":
             nbtraces_temp  = self.nbtraces
-            self.gui.nbTraces_lineEdit.show()
-            self.gui.graph_nbTracesLabel.show()
+            if not self.gui.nbTraces_lineEdit.isVisible():
+                self.gui.nbTraces_lineEdit.show()
+                self.gui.graph_nbTracesLabel.show()
         else:
             nbtraces_temp = 1  # decided to only show the last scan data for dataframes
-            self.gui.nbTraces_lineEdit.hide()
-            self.gui.graph_nbTracesLabel.hide()
+            if self.gui.nbTraces_lineEdit.isVisible():
+                self.gui.nbTraces_lineEdit.hide()
+                self.gui.graph_nbTracesLabel.hide()
 
         # Load the last results data
         data: pd.DataFrame = self.gui.dataManager.getData(
@@ -285,9 +286,11 @@ class FigureManager:
                 subdata = subdata.astype(float)
 
                 if isinstance(subdata, np.ndarray):  # is image
-                    self.fig.hide()
-                    self.figMap.show()
-                    self.gui.frame_axis.hide()
+                    if self.fig.isVisible():
+                        self.fig.hide()
+                        self.figMap.show()
+                    if self.gui.frame_axis.isVisible():
+                        self.gui.frame_axis.hide()
                     image_data[i] = subdata
                     if i == len(data)-1:
                         if image_data.ndim == 3:
@@ -299,8 +302,11 @@ class FigureManager:
                         self.figMap.setCurrentIndex(len(self.figMap.tVals))
 
                 else: # not an image (is pd.DataFrame)
-                    self.figMap.hide()
-                    self.fig.show()
+                    if not self.fig.isVisible():
+                        self.fig.show()
+                        self.figMap.hide()
+                    if not self.gui.frame_axis.isVisible():
+                        self.gui.frame_axis.show()
                     x = subdata.loc[:,variable_x]
                     y = subdata.loc[:,variable_y]
                     if isinstance(x, pd.DataFrame):