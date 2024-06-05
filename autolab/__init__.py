autolab-project/autolab

commit f612a12bf4e83e9a0d840057ac2f6cc636f85a97

Add button to download drivers

autolab/core/gui/controlcenter/main.py
@@ -26,11 +26,12 @@ from .treewidgets import TreeWidgetItemModule
 from ..scanning.main import Scanner
 from ..plotting.main import Plotter
 from ..variables import VARIABLES
-from ..GUI_utilities import get_font_size, setLineEditBackground
+from ..GUI_utilities import get_font_size
 from ..icons import icons
 from ... import devices, drivers, web, paths, config, utilities
-from .... import __version__
+from ...repository import _install_drivers_custom
 from ...web import project_url, drivers_url, doc_url
+from .... import __version__
 
 
 class OutputWrapper(QtCore.QObject):
@@ -173,10 +174,15 @@ class ControlCenter(QtWidgets.QMainWindow):
         devicesConfig.triggered.connect(self.openDevicesConfig)
         devicesConfig.setStatusTip("Open the devices configuration file")
 
-        addDeviceAction = settingsMenu.addAction('Add new device')
+        addDeviceAction = settingsMenu.addAction('Add device')
         addDeviceAction.setIcon(QtGui.QIcon(icons['add']))
         addDeviceAction.triggered.connect(self.openAddDevice)
         addDeviceAction.setStatusTip("Open the utility to add a device")
+
+        downloadDriverAction = settingsMenu.addAction('Download drivers')
+        downloadDriverAction.setIcon(QtGui.QIcon(icons['add']))
+        downloadDriverAction.triggered.connect(self.downloadDriver)
+        downloadDriverAction.setStatusTip("Open the utility to download drivers")
 
         refreshAction = settingsMenu.addAction('Refresh devices')
         refreshAction.triggered.connect(self.initialize)
@@ -476,6 +482,10 @@ class ControlCenter(QtWidgets.QMainWindow):
             self.addDevice.setWindowState(
                 self.addDevice.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
             self.addDevice.activateWindow()
+
+    def downloadDriver(self):
+        """ This function open the download driver window. """
+        _install_drivers_custom(parent=self)
 
     @staticmethod
     def openAutolabConfig():

autolab/core/repository.py
@@ -229,10 +229,14 @@ def _download_driver(url, driver_name, output_dir, _print=True):
         # 'HTTP Error 403: rate limit exceeded' due to too much download if don't have github account
         download(driver_url, output_dir=output_dir, _print=_print)
     except:  # if use Exception, crash python when having error
-        print(f"Error when downloading driver '{driver_name}'", file=sys.stderr)
+        e = f"Error when downloading driver '{driver_name}'"
+        if _print:
+            print(e, file=sys.stderr)
+        else:
+            return e
 
 
-def _install_drivers_custom(_print=True):
+def _install_drivers_custom(_print=True, parent=None):
     """ Ask the user which driver to install from the official autolab driver github repo.
     If qtpy is install, open a GUI to select the driver.
     Else, prompt the user to install individual drivers. """
@@ -243,7 +247,7 @@ def _install_drivers_custom(_print=True):
         list_driver = _get_drivers_list_from_github(official_url)
     except:
         print(f'Cannot access {official_url}, skip installation')
-        return
+        return None
 
     try:
         from qtpy import QtWidgets, QtGui
@@ -262,18 +266,21 @@ def _install_drivers_custom(_print=True):
 
         class DriverInstaller(QtWidgets.QMainWindow):
 
-            def __init__(self, url, list_driver, OUTPUT_DIR):
+            def __init__(self, url, list_driver, OUTPUT_DIR, parent=None):
                 """ GUI to select which driver to install from the official github repo """
 
+                self.gui = parent
                 self.url = url
                 self.list_driver = list_driver
                 self.OUTPUT_DIR = OUTPUT_DIR
 
-                super().__init__()
+                super().__init__(parent)
 
-                self.setWindowTitle("Autolab Driver Installer")
+                self.setWindowTitle("Autolab - Driver Installer")
                 self.setFocus()
                 self.activateWindow()
+
+                self.statusBar = self.statusBar()
 
                 centralWidget = QtWidgets.QWidget()
                 self.setCentralWidget(centralWidget)
@@ -299,8 +306,10 @@ def _install_drivers_custom(_print=True):
                 tab.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
                 tab.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
                 tab.setAlternatingRowColors(True)
-                tab.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
-                tab.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+                tab.horizontalHeader().setSectionResizeMode(
+                    0, QtWidgets.QHeaderView.ResizeToContents)
+                tab.setSizePolicy(QtWidgets.QSizePolicy.Expanding,
+                                  QtWidgets.QSizePolicy.Expanding)
                 tab.setSizeAdjustPolicy(tab.AdjustToContents)
 
                 # Init checkBox
@@ -326,27 +335,43 @@ def _install_drivers_custom(_print=True):
                 for checkBox in self.list_checkBox:
                     checkBox.setChecked(state)
 
-            def closeEvent(self, event):
-                """ This function does some steps before the window is really killed """
-                QtWidgets.QApplication.quit()  # close the interface
-
             def installListDriver(self):
                 """ Install all the drivers for which the corresponding checkBox has been checked """
+                list_bool = [
+                    checkBox.isChecked() for checkBox in self.list_checkBox]
+                list_driver_to_download = [
+                    driver_name for (driver_name, driver_bool) in zip(
+                        self.list_driver, list_bool) if driver_bool]
 
-                list_bool = [checkBox.isChecked() for checkBox in self.list_checkBox]
-                list_driver_to_download = [driver_name for (driver_name, driver_bool) in zip(self.list_driver, list_bool) if driver_bool]
+                if all(list_bool):  # Better for all drivers
+                    install_drivers(skip_input=True, experimental_feature=False)
+                    self.close()
+                elif any(list_bool):  # Better for couple drivers
+                    for driver_name in list_driver_to_download:
+                        if _print:
+                            print(f"Downloading {driver_name}")
+                            # self.setStatus(f"Downloading {driver_name}", 5000)  # OPTIMIZE: currently thread blocked by installer so don't show anything until the end
+                        e = _download_driver(self.url, driver_name,
+                                             self.OUTPUT_DIR, _print=False)
+                        if e is not None:
+                            print(e, file=sys.stderr)
+                            # self.setStatus(e, 10000, False)
+                    self.setStatus('Finished!', 5000)
 
-                for driver in list_driver_to_download:
-                    _download_driver(self.url, driver, self.OUTPUT_DIR, _print=_print)
-                print('Done!')
+            def setStatus(self, message: str, timeout: int = 0, stdout: bool = True):
+                """ Modify the message displayed in the status bar and add error message to logger """
+                self.statusBar.showMessage(message, timeout)
+                if not stdout: print(message, file=sys.stderr)
 
-        if _print: print("Open driver installer")
-        app = QtWidgets.QApplication.instance()
-        if app is None: app = QtWidgets.QApplication([])
+        if parent is None:
+            if  _print: print("Open driver installer")
+            app = QtWidgets.QApplication.instance()
+            if app is None: app = QtWidgets.QApplication([])
 
-        driverInstaller = DriverInstaller(official_url, list_driver, official_folder)
+        driverInstaller = DriverInstaller(
+            official_url, list_driver, official_folder, parent=parent)
         driverInstaller.show()
-        app.exec_()
+        if parent is None: app.exec()
 
     # Update available drivers
     drivers.update_drivers_paths()

commit f185080c9718dd2b991e2ee05041575aae9b3d52

Can open plotter directly + fixes
- Can open plotter using autolab.plotter()
- Fixe bug introduced in f612a12 driver installer not closing correctly
- Fixe bug introduced in ae66784 not displaying plotter cursors

autolab/__init__.py
@@ -54,7 +54,8 @@ from .core.web import report, doc
 from .core.server import Server as server
 
 # GUI
-from .core.gui import start as gui
+from .core.gui import gui
+from .core.gui import plotter
 
 # Repository
 from .core.repository import install_drivers

autolab/_entry_script.py
@@ -21,6 +21,7 @@ def print_help():
     print()
     print('Commands:')
     print('  gui                   Start the Graphical User Interface')
+    print('  plotter               Start the Plotter')
     print('  install_drivers       Install drivers from GitHub')
     print('  driver                Driver interface')
     print('  device                Device interface')
@@ -54,6 +55,8 @@ def main():
             autolab.report()
         elif command == 'gui':           # GUI
             autolab.gui()
+        elif command == 'plotter':           # Plotter
+            autolab.plotter()
         elif command == 'infos':
             autolab.infos()
         elif command == 'install_drivers':

autolab/core/gui/__init__.py
@@ -20,8 +20,18 @@ quentin.chateiller@c2n.upsaclay.fr
 #        t.start()
 
 
-def start():
+def gui():
     """ Open the Autolab GUI """
+    _start('main')
+
+
+def plotter():
+    """ Open the Autolab Plotter """
+    _start('plotter')
+
+
+def _start(gui: str = 'main'):
+    """ Open the Autolab GUI if gui='main' or the Autolab Plotter if gui='plotter' """
 
     import os
     from ..config import get_GUI_config
@@ -66,9 +76,15 @@ conda install -c conda-forge pyside6
             font.setPointSize(int(GUI_config['font_size']))
             app.setFont(font)
 
-        from .controlcenter.main import ControlCenter
-        gui = ControlCenter()
-        gui.initialize()
+        if gui == 'main':
+            from .controlcenter.main import ControlCenter
+            gui = ControlCenter()
+            gui.initialize()
+        elif gui == 'plotter':
+            from .plotting.main import Plotter
+            gui = Plotter(None)
+        else:
+            raise ValueError(f"gui accept either 'main' or 'plotter', given {gui}")
         gui.show()
         app.exec()
 

autolab/core/gui/controlcenter/main.py
@@ -18,7 +18,6 @@ import numpy as np
 import pandas as pd
 import qtpy
 from qtpy import QtCore, QtWidgets, QtGui
-from qtpy.QtWidgets import QApplication
 import pyqtgraph as pg
 
 from .thread import ThreadManager
@@ -578,7 +577,7 @@ class ControlCenter(QtWidgets.QMainWindow):
 
         devices.close()  # close all devices
 
-        QApplication.quit()  # close the control center interface
+        QtWidgets.QApplication.quit()  # close the control center interface
 
         if hasattr(self, 'stdout'):
             sys.stdout = self.stdout._stream

autolab/core/gui/plotting/interface.ui
@@ -154,7 +154,7 @@
             </item>
            </layout>
           </widget>
-          <widget class="QFrame" name="frame5">
+          <widget class="QFrame" name="frame_device">
            <property name="frameShape">
             <enum>QFrame::StyledPanel</enum>
            </property>

autolab/core/gui/plotting/main.py
@@ -108,30 +108,34 @@ class Plotter(QtWidgets.QMainWindow):
         self.variable_y_comboBox.currentIndexChanged.connect(
             self.variableChanged)
 
-        self.device_lineEdit.setText(f'{self.dataManager.deviceValue}')
-        self.device_lineEdit.returnPressed.connect(self.deviceChanged)
-        self.device_lineEdit.textEdited.connect(lambda: setLineEditBackground(
-            self.device_lineEdit, 'edited', self._font_size))
-        setLineEditBackground(self.device_lineEdit, 'synced', self._font_size)
+        if self.mainGui is not None:
+            self.device_lineEdit.setText(f'{self.dataManager.deviceValue}')
+            self.device_lineEdit.returnPressed.connect(self.deviceChanged)
+            self.device_lineEdit.textEdited.connect(lambda: setLineEditBackground(
+                self.device_lineEdit, 'edited', self._font_size))
+            setLineEditBackground(self.device_lineEdit, 'synced', self._font_size)
 
-        # Plot button
-        self.plotDataButton.clicked.connect(self.refreshPlotData)
+            # Plot button
+            self.plotDataButton.clicked.connect(self.refreshPlotData)
 
-        # Timer
-        self.timer_time = 0.5  # This plotter is not meant for fast plotting like the monitor, be aware it may crash with too high refreshing rate
-        self.timer = QtCore.QTimer(self)
-        self.timer.setInterval(int(self.timer_time*1000)) # ms
-        self.timer.timeout.connect(self.autoRefreshPlotData)
+            # Timer
+            self.timer_time = 0.5  # This plotter is not meant for fast plotting like the monitor, be aware it may crash with too high refreshing rate
+            self.timer = QtCore.QTimer(self)
+            self.timer.setInterval(int(self.timer_time*1000)) # ms
+            self.timer.timeout.connect(self.autoRefreshPlotData)
 
-        self.auto_plotDataButton.clicked.connect(self.autoRefreshChanged)
+            self.auto_plotDataButton.clicked.connect(self.autoRefreshChanged)
+
+            # Delay
+            self.delay_lineEdit.setText(str(self.timer_time))
+            self.delay_lineEdit.returnPressed.connect(self.delayChanged)
+            self.delay_lineEdit.textEdited.connect(lambda: setLineEditBackground(
+                self.delay_lineEdit, 'edited', self._font_size))
+            setLineEditBackground(self.delay_lineEdit, 'synced', self._font_size)
+        else:
+            self.frame_device.hide()
+
         self.overwriteDataButton.clicked.connect(self.overwriteDataChanged)
-
-        # Delay
-        self.delay_lineEdit.setText(str(self.timer_time))
-        self.delay_lineEdit.returnPressed.connect(self.delayChanged)
-        self.delay_lineEdit.textEdited.connect(lambda: setLineEditBackground(
-            self.delay_lineEdit, 'edited', self._font_size))
-        setLineEditBackground(self.delay_lineEdit, 'synced', self._font_size)
 
         self.setAcceptDrops(True)
 
@@ -185,8 +189,8 @@ class Plotter(QtWidgets.QMainWindow):
             if action == 'create':
                 widget = widget(*args, **kwargs)
                 self.dict_widget[widget_name] = widget
-                try: self.figureManager.fig.addItem(widget)
-                except: pass
+                try: self.figureManager.ax.addItem(widget)
+                except Exception as e: self.setStatus(str(e), 10000, False)
             elif action == "remove":
                 d = self.dict_widget
                 if widget is not None:
@@ -196,8 +200,8 @@ class Plotter(QtWidgets.QMainWindow):
                         widget = d.get(widget_name)
                         if widget is not None:
                             widget = d.pop(widget_name)
-                            try: self.figureManager.fig.removeItem(widget)
-                            except: pass
+                            try: self.figureManager.ax.removeItem(widget)
+                            except Exception as e: self.setStatus(str(e), 10000, False)
 
     def timerAction(self):
         """ This function checks if a module has been loaded and put to the queue. If so, associate item and module """
@@ -448,11 +452,17 @@ class Plotter(QtWidgets.QMainWindow):
 
     def closeEvent(self,event):
         """ This function does some steps before the window is closed (not killed) """
-        self.timer.stop()
+        if hasattr(self, 'timer'): self.timer.stop()
         self.timerPlugin.stop()
         self.timerQueue.stop()
 
-        self.mainGui.clearPlotter()
+        if hasattr(self.mainGui, 'clearPlotter'):
+            self.mainGui.clearPlotter()
+
+        super().closeEvent(event)
+
+        if self.mainGui is None:
+            QtWidgets.QApplication.quit()  # close the plotter app
 
     def close(self):
         """ This function does some steps before the window is killed """

autolab/core/repository.py
@@ -358,6 +358,13 @@ def _install_drivers_custom(_print=True, parent=None):
                             # self.setStatus(e, 10000, False)
                     self.setStatus('Finished!', 5000)
 
+            def closeEvent(self, event):
+                """ This function does some steps before the window is really killed """
+                super().closeEvent(event)
+
+                if self.gui is None:
+                    QtWidgets.QApplication.quit()  # close the app
+
             def setStatus(self, message: str, timeout: int = 0, stdout: bool = True):
                 """ Modify the message displayed in the status bar and add error message to logger """
                 self.statusBar.showMessage(message, timeout)