autolab-project/autolab

commit 198d63373d8903765bc4e24341e2586437599b52

Can now cancel a device connection
Can now right click on device to cancel the connection if takes too long -> avoid blocking the GUI

autolab/core/gui/controlcenter/main.py
@@ -365,6 +365,12 @@ class ControlCenter(QtWidgets.QMainWindow):
             self.threadManager.start(item, 'load')  # load device and add it to queue for timer to associate it later (doesn't block gui while device is openning)
             self.timerDevice.start()
 
+    def itemCanceled(self, item):
+        """ Cancel the device openning. Can be used to avoid GUI blocking for devices with infinite loading issue """
+        if id(item) in self.threadManager.threads:
+            self.threadManager.threads[id(item)].endSignal.emit(f'Cancel loading device {item.name}')
+            self.threadManager.threads[id(item)].terminate()
+
     def itemPressed(self, item: QtWidgets.QTreeWidgetItem):
         """ Function called when a click (not released) has been detected in the tree.
             Store last dragged variable in tree so scanner can know it when it is dropped there.

autolab/core/gui/controlcenter/thread.py
@@ -53,8 +53,9 @@ class ThreadManager:
         self.gui.setStatus(status)
 
         # Thread configuration
+        tid = id(item)
+        assert tid not in self.threads
         thread = InteractionThread(item, intType, value)
-        tid = id(thread)
         self.threads[tid] = thread
         thread.endSignal.connect(
             lambda error, x=tid: self.threadFinished(x, error))
@@ -65,9 +66,14 @@ class ThreadManager:
 
     def threadFinished(self, tid: int, error: Exception):
         """ This function is called when a thread has finished its job, with an error or not
-        It updates the status bar of the GUI in consequence and enabled back the correspondig item """
-        if error is None: self.gui.clearStatus()
-        else: self.gui.setStatus(str(error), 10000, False)
+        It updates the status bar of the GUI in consequence and enabled back the corresponding item """
+        if error:
+            self.gui.setStatus(str(error), 10000, False)
+
+            if tid in self.gui.threadItemDict:
+                self.gui.threadItemDict.pop(tid)
+        else:
+            self.gui.clearStatus()
 
         item = self.threads[tid].item
         item.setDisabled(False)
@@ -143,7 +149,5 @@ class InteractionThread(QtCore.QThread):
             error = e
             if self.intType == 'load':
                 error = f'An error occured when loading device {self.item.name}: {str(e)}'
-                if id(self.item) in self.item.gui.threadItemDict.keys():
-                    self.item.gui.threadItemDict.pop(id(self.item))
 
         self.endSignal.emit(error)

autolab/core/gui/controlcenter/treewidgets.py
@@ -74,20 +74,30 @@ class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
 
     def menu(self, position: QtCore.QPoint):
         """ This function provides the menu when the user right click on an item """
-        if self.is_not_submodule and self.loaded:
-            menu = QtWidgets.QMenu()
-            disconnectDevice = menu.addAction(f"Disconnect {self.name}")
-            disconnectDevice.setIcon(QtGui.QIcon(icons['disconnect']))
+        if self.is_not_submodule:
+            if self.loaded:
+                menu = QtWidgets.QMenu()
+                disconnectDevice = menu.addAction(f"Disconnect {self.name}")
+                disconnectDevice.setIcon(QtGui.QIcon(icons['disconnect']))
 
-            choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
+                choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
 
-            if choice == disconnectDevice:
-                close(self.name)
+                if choice == disconnectDevice:
+                    close(self.name)
 
-                for i in range(self.childCount()):
-                    self.removeChild(self.child(0))
+                    for i in range(self.childCount()):
+                        self.removeChild(self.child(0))
 
-                self.loaded = False
+                    self.loaded = False
+            elif id(self) in self.gui.threadManager.threads:
+                menu = QtWidgets.QMenu()
+                cancelDevice = menu.addAction(f"Cancel loading")
+                cancelDevice.setIcon(QtGui.QIcon(icons['disconnect']))
+
+                choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
+
+                if choice == cancelDevice:
+                    self.gui.itemCanceled(self)
 
 
 class TreeWidgetItemAction(QtWidgets.QTreeWidgetItem):

commit 0e9dd321c231ae7bc761f8c205c4cd979bdaa5ec

Fixe thread issue, add doc
- fixe issue introduced in 198d633 preventing to have multiple threads coming from the same tree item at the same time
- remove signals from Variable on device closure (avoid c++ errors)
- now check qt_object_exists(statusbar) in thread (avoid c++ errors)
- add docs

autolab/core/devices.py
@@ -9,7 +9,7 @@ from typing import List, Union
 
 from . import drivers
 from . import config
-from .elements import Module
+from .elements import Module, Element
 
 # Storage of the devices
 DEVICES = {}
@@ -32,8 +32,20 @@ class Device(Module):
 
     def close(self):
         """ This function close the connection of the current physical device """
+        # Remove read and write signals from gui
+        try:
+            # condition avoid reopenning connection if use close twice
+            if self.name in DEVICES:
+                for struc in self.get_structure():
+                    element = get_element_by_address(struc[0])
+                    if struc[1] == 'variable':
+                        element._read_signal = None
+                        element._write_signal = None
+        except: pass
+
         try: self.instance.close()
         except: pass
+
         del DEVICES[self.name]
 
     def __dir__(self):
@@ -46,8 +58,8 @@ class Device(Module):
 # DEVICE GET FUNCTION
 # =============================================================================
 
-def get_device_by_address(address: str) -> Union[Device, None]:
-    """ Returns the Device located at the provided address if exists """
+def get_element_by_address(address: str) -> Union[Element, None]:
+    """ Returns the Element located at the provided address if exists """
     address = address.split('.')
     try:
         element = get_device(address[0])

autolab/core/drivers.py
@@ -139,7 +139,7 @@ def get_driver_category(driver_name: str) -> str:
 
         driver_utilities_path = os.path.join(
             os.path.dirname(get_driver_path(driver_name)), f'{driver_name}{filename}.py')
-        category = 'Other'
+        category = 'Unknown'
 
         if os.path.exists(driver_utilities_path):
             try:

autolab/core/gui/__init__.py
@@ -52,6 +52,7 @@ def monitor(var):
 
 
 def slider(var):
+    """ Open a slider for variable var """
     item = _create_item(var)
     _start('slider', item=item)
 

autolab/core/gui/controlcenter/main.py
@@ -389,16 +389,17 @@ class ControlCenter(QtWidgets.QMainWindow):
         """ Function called when a normal click has been detected in the tree.
             Check the association if it is a main item """
         if (item.parent() is None and not item.loaded
-                and id(item) not in self.threadItemDict.keys()):
+                and id(item) not in self.threadItemDict):
             self.threadItemDict[id(item)] = item  # needed before start of timer to avoid bad timing and to stop thread before loading is done
             self.threadManager.start(item, 'load')  # load device and add it to queue for timer to associate it later (doesn't block gui while device is openning)
             self.timerDevice.start()
 
     def itemCanceled(self, item):
         """ Cancel the device openning. Can be used to avoid GUI blocking for devices with infinite loading issue """
-        if id(item) in self.threadManager.threads:
-            self.threadManager.threads[id(item)].endSignal.emit(f'Cancel loading device {item.name}')
-            self.threadManager.threads[id(item)].terminate()
+        if id(item) in self.threadManager.threads_conn:
+            tid = self.threadManager.threads_conn[id(item)]
+            self.threadManager.threads[tid].endSignal.emit(f'Cancel loading device {item.name}')
+            self.threadManager.threads[tid].terminate()
 
     def itemPressed(self, item: QtWidgets.QTreeWidgetItem):
         """ Function called when a click (not released) has been detected in the tree.

autolab/core/gui/controlcenter/thread.py
@@ -5,6 +5,7 @@ Created on Sun Sep 29 18:26:32 2019
 @author: qchat
 """
 
+import sys
 import inspect
 from typing import Any
 
@@ -20,6 +21,7 @@ class ThreadManager:
     def __init__(self, gui: QtWidgets.QMainWindow):
         self.gui = gui
         self.threads = {}
+        self.threads_conn = {}
 
     def start(self, item: QtWidgets.QTreeWidgetItem, intType: str, value = None):
         """ This function is called when a new thread is requested,
@@ -53,10 +55,14 @@ class ThreadManager:
         self.gui.setStatus(status)
 
         # Thread configuration
-        tid = id(item)
-        assert tid not in self.threads
+        if intType == 'load':
+            assert id(item) not in self.threads_conn
         thread = InteractionThread(item, intType, value)
+        tid = id(thread)
         self.threads[tid] = thread
+        if intType == 'load':
+            self.threads_conn[id(item)] = tid
+
         thread.endSignal.connect(
             lambda error, x=tid: self.threadFinished(x, error))
         thread.finished.connect(lambda x=tid: self.delete(x))
@@ -68,15 +74,22 @@ class ThreadManager:
         """ This function is called when a thread has finished its job, with an error or not
         It updates the status bar of the GUI in consequence and enabled back the corresponding item """
         if error:
-            self.gui.setStatus(str(error), 10000, False)
+            if qt_object_exists(self.gui.statusBar):
+                self.gui.setStatus(str(error), 10000, False)
+            else:
+                print(str(error), file=sys.stderr)
 
-            if tid in self.gui.threadItemDict:
-                self.gui.threadItemDict.pop(tid)
+            if tid in self.threads_conn.values():
+                item_id = list(self.threads_conn)[list(self.threads_conn.values()).index(tid)]
+                if item_id in self.gui.threadItemDict:
+                    self.gui.threadItemDict.pop(item_id)
         else:
-            self.gui.clearStatus()
+            if qt_object_exists(self.gui.statusBar):
+                self.gui.clearStatus()
 
         item = self.threads[tid].item
-        item.setDisabled(False)
+        if qt_object_exists(item):
+            item.setDisabled(False)
 
         if hasattr(item, "execButton"):
             if qt_object_exists(item.execButton):
@@ -91,6 +104,9 @@ class ThreadManager:
     def delete(self, tid: int):
         """ This function is called when a thread is about to be deleted.
         This removes it from the dictionnary self.threads, for a complete deletion """
+        if self.threads[tid].intType == 'load':
+            item_id = list(self.threads_conn)[list(self.threads_conn.values()).index(tid)]
+            self.threads_conn.pop(item_id)
         self.threads.pop(tid)
 
 

autolab/core/gui/controlcenter/treewidgets.py
@@ -89,7 +89,7 @@ class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
                         self.removeChild(self.child(0))
 
                     self.loaded = False
-            elif id(self) in self.gui.threadManager.threads:
+            elif id(self) in self.gui.threadManager.threads_conn:
                 menu = QtWidgets.QMenu()
                 cancelDevice = menu.addAction(f"Cancel loading")
                 cancelDevice.setIcon(QtGui.QIcon(icons['disconnect']))

autolab/core/gui/scanning/config.py
@@ -930,7 +930,7 @@ class ConfigManager:
                     assert 'address' in param_pars, f"Missing address to {param_pars}"
                     if param_pars['address'] == "None": element = None
                     else:
-                        element = devices.get_device_by_address(param_pars['address'])
+                        element = devices.get_element_by_address(param_pars['address'])
                         assert element is not None, f"Parameter {param_pars['address']} not found."
 
                     param['element'] = element
@@ -982,7 +982,7 @@ class ConfigManager:
                             assert step['stepType'] != 'recipe', "Removed the recipe in recipe feature!"
                             element = address
                         else:
-                            element = devices.get_device_by_address(address)
+                            element = devices.get_element_by_address(address)
 
                         assert element is not None, f"Address {address} not found for step {i} ({name})."
                         step['element'] = element