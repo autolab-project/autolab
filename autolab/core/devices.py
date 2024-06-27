autolab-project/autolab

commit 0e9dd321c231ae7bc761f8c205c4cd979bdaa5ec

Fixe thread issue, add doc
- fixe issue introduced in 198d633 preventing to have multiple threads coming from the same tree item at the same time
- remove signals from Variable on device closure (avoid c++ errors)
- now check qt_object_exists(statusbar) in thread (avoid c++ errors)
- add docs

autolab/core/devices.py
@@ -9,7 +9,7 @@ from typing import List, Union
 
 from . import drivers
 from . import config
-from .elements import Module
+from .elements import Module, Element
 
 # Storage of the devices
 DEVICES = {}
@@ -32,8 +32,20 @@ class Device(Module):
 
     def close(self):
         """ This function close the connection of the current physical device """
+        # Remove read and write signals from gui
+        try:
+            # condition avoid reopenning connection if use close twice
+            if self.name in DEVICES:
+                for struc in self.get_structure():
+                    element = get_element_by_address(struc[0])
+                    if struc[1] == 'variable':
+                        element._read_signal = None
+                        element._write_signal = None
+        except: pass
+
         try: self.instance.close()
         except: pass
+
         del DEVICES[self.name]
 
     def __dir__(self):
@@ -46,8 +58,8 @@ class Device(Module):
 # DEVICE GET FUNCTION
 # =============================================================================
 
-def get_device_by_address(address: str) -> Union[Device, None]:
-    """ Returns the Device located at the provided address if exists """
+def get_element_by_address(address: str) -> Union[Element, None]:
+    """ Returns the Element located at the provided address if exists """
     address = address.split('.')
     try:
         element = get_device(address[0])

autolab/core/gui/controlcenter/treewidgets.py
@@ -89,7 +89,7 @@ class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
                         self.removeChild(self.child(0))
 
                     self.loaded = False
-            elif id(self) in self.gui.threadManager.threads:
+            elif id(self) in self.gui.threadManager.threads_conn:
                 menu = QtWidgets.QMenu()
                 cancelDevice = menu.addAction(f"Cancel loading")
                 cancelDevice.setIcon(QtGui.QIcon(icons['disconnect']))

autolab/core/drivers.py
@@ -139,7 +139,7 @@ def get_driver_category(driver_name: str) -> str:
 
         driver_utilities_path = os.path.join(
             os.path.dirname(get_driver_path(driver_name)), f'{driver_name}{filename}.py')
-        category = 'Other'
+        category = 'Unknown'
 
         if os.path.exists(driver_utilities_path):
             try:

autolab/core/gui/__init__.py
@@ -52,6 +52,7 @@ def monitor(var):
 
 
 def slider(var):
+    """ Open a slider for variable var """
     item = _create_item(var)
     _start('slider', item=item)
 

autolab/core/gui/controlcenter/main.py
@@ -389,16 +389,17 @@ class ControlCenter(QtWidgets.QMainWindow):
         """ Function called when a normal click has been detected in the tree.
             Check the association if it is a main item """
         if (item.parent() is None and not item.loaded
-                and id(item) not in self.threadItemDict.keys()):
+                and id(item) not in self.threadItemDict):
             self.threadItemDict[id(item)] = item  # needed before start of timer to avoid bad timing and to stop thread before loading is done
             self.threadManager.start(item, 'load')  # load device and add it to queue for timer to associate it later (doesn't block gui while device is openning)
             self.timerDevice.start()
 
     def itemCanceled(self, item):
         """ Cancel the device openning. Can be used to avoid GUI blocking for devices with infinite loading issue """
-        if id(item) in self.threadManager.threads:
-            self.threadManager.threads[id(item)].endSignal.emit(f'Cancel loading device {item.name}')
-            self.threadManager.threads[id(item)].terminate()
+        if id(item) in self.threadManager.threads_conn:
+            tid = self.threadManager.threads_conn[id(item)]
+            self.threadManager.threads[tid].endSignal.emit(f'Cancel loading device {item.name}')
+            self.threadManager.threads[tid].terminate()
 
     def itemPressed(self, item: QtWidgets.QTreeWidgetItem):
         """ Function called when a click (not released) has been detected in the tree.

autolab/core/gui/controlcenter/thread.py
@@ -5,6 +5,7 @@ Created on Sun Sep 29 18:26:32 2019
 @author: qchat
 """
 
+import sys
 import inspect
 from typing import Any
 
@@ -20,6 +21,7 @@ class ThreadManager:
     def __init__(self, gui: QtWidgets.QMainWindow):
         self.gui = gui
         self.threads = {}
+        self.threads_conn = {}
 
     def start(self, item: QtWidgets.QTreeWidgetItem, intType: str, value = None):
         """ This function is called when a new thread is requested,
@@ -53,10 +55,14 @@ class ThreadManager:
         self.gui.setStatus(status)
 
         # Thread configuration
-        tid = id(item)
-        assert tid not in self.threads
+        if intType == 'load':
+            assert id(item) not in self.threads_conn
         thread = InteractionThread(item, intType, value)
+        tid = id(thread)
         self.threads[tid] = thread
+        if intType == 'load':
+            self.threads_conn[id(item)] = tid
+
         thread.endSignal.connect(
             lambda error, x=tid: self.threadFinished(x, error))
         thread.finished.connect(lambda x=tid: self.delete(x))
@@ -68,15 +74,22 @@ class ThreadManager:
         """ This function is called when a thread has finished its job, with an error or not
         It updates the status bar of the GUI in consequence and enabled back the corresponding item """
         if error:
-            self.gui.setStatus(str(error), 10000, False)
+            if qt_object_exists(self.gui.statusBar):
+                self.gui.setStatus(str(error), 10000, False)
+            else:
+                print(str(error), file=sys.stderr)
 
-            if tid in self.gui.threadItemDict:
-                self.gui.threadItemDict.pop(tid)
+            if tid in self.threads_conn.values():
+                item_id = list(self.threads_conn)[list(self.threads_conn.values()).index(tid)]
+                if item_id in self.gui.threadItemDict:
+                    self.gui.threadItemDict.pop(item_id)
         else:
-            self.gui.clearStatus()
+            if qt_object_exists(self.gui.statusBar):
+                self.gui.clearStatus()
 
         item = self.threads[tid].item
-        item.setDisabled(False)
+        if qt_object_exists(item):
+            item.setDisabled(False)
 
         if hasattr(item, "execButton"):
             if qt_object_exists(item.execButton):
@@ -91,6 +104,9 @@ class ThreadManager:
     def delete(self, tid: int):
         """ This function is called when a thread is about to be deleted.
         This removes it from the dictionnary self.threads, for a complete deletion """
+        if self.threads[tid].intType == 'load':
+            item_id = list(self.threads_conn)[list(self.threads_conn.values()).index(tid)]
+            self.threads_conn.pop(item_id)
         self.threads.pop(tid)
 
 

autolab/core/gui/scanning/config.py
@@ -930,7 +930,7 @@ class ConfigManager:
                     assert 'address' in param_pars, f"Missing address to {param_pars}"
                     if param_pars['address'] == "None": element = None
                     else:
-                        element = devices.get_device_by_address(param_pars['address'])
+                        element = devices.get_element_by_address(param_pars['address'])
                         assert element is not None, f"Parameter {param_pars['address']} not found."
 
                     param['element'] = element
@@ -982,7 +982,7 @@ class ConfigManager:
                             assert step['stepType'] != 'recipe', "Removed the recipe in recipe feature!"
                             element = address
                         else:
-                            element = devices.get_device_by_address(address)
+                            element = devices.get_element_by_address(address)
 
                         assert element is not None, f"Address {address} not found for step {i} ({name})."
                         step['element'] = element

commit 58bd62be8157eb8a9fef6a79fb5b4ddef3971b82

Can select active recipe and parameter from controlpanel
- Add sub-menu to select recipe and parameter in controlpanel actions
- fixe access violation on GUI exit (bug introduced in 0e9dd32 due to get_element_by_address on device close)
- doc

autolab/core/devices.py
@@ -62,7 +62,12 @@ def get_element_by_address(address: str) -> Union[Element, None]:
     """ Returns the Element located at the provided address if exists """
     address = address.split('.')
     try:
-        element = get_device(address[0])
+        device_name = address[0]
+        if device_name in DEVICES:
+            element = DEVICES[device_name]
+        else:
+            # This should not be used on autolab closing to avoid access violation due to config opening
+            element = get_device(device_name)
         for addressPart in address[1: ]:
             element = getattr(element, addressPart.replace(" ", ""))
         return element

autolab/core/gui/controlcenter/treewidgets.py
@@ -7,7 +7,7 @@ Created on Sun Sep 29 18:29:07 2019
 
 
 import os
-from typing import Any
+from typing import Any, Union
 
 import pandas as pd
 import numpy as np
@@ -23,6 +23,151 @@ from ...devices import close
 from ...utilities import (SUPPORTED_EXTENSION,
                           str_to_array, array_to_str,
                           dataframe_to_str, str_to_dataframe)
+
+
+class CustomMenu(QtWidgets.QMenu):
+    """ Menu with action containing sub-menu for recipe and parameter selection """
+
+    def __init__(self, gui):
+        super().__init__()
+        self.gui = gui
+        self.current_menu = 1
+        self.selected_action = None
+
+        self.recipe_cb = self.gui.scanner.selectRecipe_comboBox if (
+            self.gui.scanner) else None
+        self.recipe_names = [self.recipe_cb.itemText(i) for i in range(
+            self.recipe_cb.count())] if self.recipe_cb else []
+        self.param_cb = self.gui.scanner.selectParameter_comboBox if (
+            self.gui.scanner) else None
+
+        self.CUSTOM_ACTION = len(self.recipe_names) > 1
+
+    def addAnyAction(self, action_text='', icon_name='',
+                     param_menu_active=False) -> Union[QtWidgets.QWidgetAction,
+                                                       QtWidgets.QAction]:
+        if self.CUSTOM_ACTION:
+            action = self.addCustomAction(action_text, icon_name,
+                                          param_menu_active=param_menu_active)
+        else:
+            action = self.addAction(action_text)
+            if icon_name != '':
+                action.setIcon(QtGui.QIcon(icons[icon_name]))
+
+        return action
+
+    def addCustomAction(self, action_text='', icon_name='',
+                        param_menu_active=False) -> QtWidgets.QWidgetAction:
+        """ Create an action with a sub menu for selecting a recipe and parameter """
+
+        def close_menu():
+            self.selected_action = action_widget
+            self.close()
+
+        def handle_hover():
+            """ Fixe bad hover behavior and refresh radio_button """
+            self.setActiveAction(action_widget)
+            recipe_name = self.recipe_cb.currentText()
+            action = recipe_menu.actions()[self.recipe_names.index(recipe_name)]
+            radio_button = action.defaultWidget()
+
+            if not radio_button.isChecked():
+                radio_button.setChecked(True)
+
+        def handle_radio_click(name):
+            """ Update parameters available, open parameter menu if available
+            and close main menu to validate the action """
+            if self.current_menu == 1:
+                self.recipe_cb.setCurrentIndex(self.recipe_names.index(name))
+                self.gui.scanner._updateSelectParameter()
+                recipe_menu.close()
+
+                if param_menu_active:
+                    param_items = [self.param_cb.itemText(i) for i in range(
+                        self.param_cb.count())]
+
+                    if len(param_items) > 1:
+                        self.current_menu = 2
+                        setup_menu_parameter(param_menu)
+                        return None
+            else:
+                update_parameter(name)
+                param_menu.close()
+                self.current_menu = 1
+                action_button.setMenu(recipe_menu)
+
+            close_menu()
+
+        def reset_menu(button: QtWidgets.QToolButton):
+            QtWidgets.QApplication.sendEvent(
+                button, QtCore.QEvent(QtCore.QEvent.Leave))
+            self.current_menu = 1
+            action_button.setMenu(recipe_menu)
+
+        def setup_menu_parameter(param_menu: QtWidgets.QMenu):
+            param_items = [self.param_cb.itemText(i) for i in range(
+                self.param_cb.count())]
+            param_name = self.param_cb.currentText()
+
+            param_menu.clear()
+            for param_name_i in param_items:
+                add_radio_button_to_menu(param_name_i, param_name, param_menu)
+
+            action_button.setMenu(param_menu)
+            action_button.showMenu()
+
+        def update_parameter(name: str):
+            param_items = [self.param_cb.itemText(i) for i in range(
+                self.param_cb.count())]
+            self.param_cb.setCurrentIndex(param_items.index(name))
+            self.gui.scanner._updateSelectParameter()
+
+        def add_radio_button_to_menu(item_name: str, current_name: str,
+                                     target_menu: QtWidgets.QMenu):
+            widget = QtWidgets.QWidget()
+            radio_button = QtWidgets.QRadioButton(item_name, widget)
+            action = QtWidgets.QWidgetAction(self.gui)
+            action.setDefaultWidget(radio_button)
+            target_menu.addAction(action)
+
+            if item_name == current_name:
+                radio_button.setChecked(True)
+
+            radio_button.clicked.connect(
+                lambda: handle_radio_click(item_name))
+
+        # Add custom action
+        action_button = QtWidgets.QToolButton()
+        action_button.setPopupMode(QtWidgets.QToolButton.MenuButtonPopup)
+        action_button.setText(f"   {action_text}")
+        action_button.setToolButtonStyle(QtCore.Qt.ToolButtonTextBesideIcon)
+        action_button.setAutoRaise(True)
+        action_button.clicked.connect(close_menu)
+        action_button.enterEvent = lambda event: handle_hover()
+        if icon_name != '':
+            action_button.setIcon(QtGui.QIcon(icons[icon_name]))
+
+        action_widget = QtWidgets.QWidgetAction(action_button)
+        action_widget.setDefaultWidget(action_button)
+        self.addAction(action_widget)
+
+        recipe_menu = QtWidgets.QMenu()
+        # recipe_menu.aboutToShow.connect(lambda: self.set_clickable(False))
+        recipe_menu.aboutToHide.connect(lambda: reset_menu(action_button))
+
+        if param_menu_active:
+            param_menu = QtWidgets.QMenu()
+            # param_menu.aboutToShow.connect(lambda: self.set_clickable(False))
+            param_menu.aboutToHide.connect(lambda: reset_menu(action_button))
+
+        recipe_name = self.gui.scanner.selectRecipe_comboBox.currentText()
+
+        for recipe_name_i in self.recipe_names:
+            add_radio_button_to_menu(recipe_name_i, recipe_name, recipe_menu)
+
+        action_button.setMenu(recipe_menu)
+
+        return action_widget
 
 
 class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
@@ -91,7 +236,7 @@ class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
                     self.loaded = False
             elif id(self) in self.gui.threadManager.threads_conn:
                 menu = QtWidgets.QMenu()
-                cancelDevice = menu.addAction(f"Cancel loading")
+                cancelDevice = menu.addAction('Cancel loading')
                 cancelDevice.setIcon(QtGui.QIcon(icons['disconnect']))
 
                 choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
@@ -107,6 +252,7 @@ class TreeWidgetItemModule(QtWidgets.QTreeWidgetItem):
 
                 if choice == modifyDeviceChoice:
                     self.gui.openAddDevice(self)
+
 
 class TreeWidgetItemAction(QtWidgets.QTreeWidgetItem):
     """ This class represents an action in an item of the tree """
@@ -227,11 +373,13 @@ class TreeWidgetItemAction(QtWidgets.QTreeWidgetItem):
     def menu(self, position: QtCore.QPoint):
         """ This function provides the menu when the user right click on an item """
         if not self.isDisabled():
-            menu = QtWidgets.QMenu()
-            scanRecipe = menu.addAction("Do in scan recipe")
-            scanRecipe.setIcon(QtGui.QIcon(icons['action']))
+            menu = CustomMenu(self.gui)
+
+            scanRecipe = menu.addAnyAction('Do in scan recipe', 'action')
 
             choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
+            if choice is None: choice = menu.selected_action
+
             if choice == scanRecipe:
                 recipe_name = self.gui.getRecipeName()
                 self.gui.addStepToScanRecipe(recipe_name, 'action', self.action)
@@ -491,36 +639,21 @@ class TreeWidgetItemVariable(QtWidgets.QTreeWidgetItem):
     def menu(self, position: QtCore.QPoint):
         """ This function provides the menu when the user right click on an item """
         if not self.isDisabled():
-            # TODO: could be used to select which recipe and parameter the step should go
-            # But, seems not ergonomic (not finish to implement it if want it)
-            # if self.gui.scanner is None:
-            #     pass
-            # else:
-            #     recipe_name = self.gui.getRecipeName()
-            #     recipe_name_list = self.gui.scanner.configManager.recipeNameList()
-            #     param_name = self.gui.getParameterName()
-            #     param_name_list = self.gui.scanner.configManager.parameterNameList(recipe_name)
-            #     print(recipe_name, recipe_name_list,
-            #           param_name, param_name_list)
-
-            menu = QtWidgets.QMenu()
+            menu = CustomMenu(self.gui)
             monitoringAction = menu.addAction("Start monitoring")
             monitoringAction.setIcon(QtGui.QIcon(icons['monitor']))
             menu.addSeparator()
             sliderAction = menu.addAction("Create a slider")
             sliderAction.setIcon(QtGui.QIcon(icons['slider']))
             menu.addSeparator()
-            # sub_menu = QtWidgets.QMenu("Set as parameter", menu)
-            # sub_menu.setIcon(QtGui.QIcon(icons['parameter']))
-            # menu.addMenu(sub_menu)
-            # scanParameterAction = sub_menu.addAction(f"in {recipe_name}")
-            # scanParameterAction.setIcon(QtGui.QIcon(icons['recipe']))
-            scanParameterAction = menu.addAction("Set as scan parameter")
-            scanParameterAction.setIcon(QtGui.QIcon(icons['parameter']))
-            scanMeasureStepAction = menu.addAction("Measure in scan recipe")
-            scanMeasureStepAction.setIcon(QtGui.QIcon(icons['measure']))
-            scanSetStepAction = menu.addAction("Set value in scan recipe")
-            scanSetStepAction.setIcon(QtGui.QIcon(icons['write']))
+
+            scanParameterAction = menu.addAnyAction(
+                'Set as scan parameter', 'parameter', param_menu_active=True)
+            scanMeasureStepAction = menu.addAnyAction(
+                'Measure in scan recipe', 'measure')
+            scanSetStepAction = menu.addAnyAction(
+                'Set value in scan recipe', 'write')
+
             menu.addSeparator()
             saveAction = menu.addAction("Read and save as...")
             saveAction.setIcon(QtGui.QIcon(icons['read-save']))
@@ -539,6 +672,8 @@ class TreeWidgetItemVariable(QtWidgets.QTreeWidgetItem):
                     tuple] else False)  # OPTIMIZE: forbid setting tuple to scanner
 
             choice = menu.exec_(self.gui.tree.viewport().mapToGlobal(position))
+            if choice is None: choice = menu.selected_action
+
             if choice == monitoringAction: self.openMonitor()
             elif choice == sliderAction: self.openSlider()
             elif choice == scanParameterAction: